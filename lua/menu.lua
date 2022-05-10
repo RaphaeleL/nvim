55122764bd491050972f3aab955d961ce2a2bc2d
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-05-10 17:48:24 +0200

    Upgraded Neovim

--- lua/menu.lua
+++ lua/menu.lua
@@ -1,44 +1,106 @@
 local menu = require("which-key")
 
+menu.setup({
+  plugins = {
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    spelling = {
+      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 20, -- how many suggestions should be shown in the list?
+    },
+    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+    -- No actual key bindings are created
+    presets = {
+      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = true, -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
+      windows = true, -- default bindings on <c-w>
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
+  },
+  -- add operators that will trigger motion and text object completion
+  -- to enable all native operators, set the preset / operators plugin above
+  -- operators = { gc = "Comments" },
+  key_labels = {
+    -- override the label used to display some keys. It doesn't effect WK in any other way.
+    -- For example:
+    -- ["<space>"] = "SPC",
+    -- ["<cr>"] = "RET",
+    -- ["<tab>"] = "TAB",
+  },
+  icons = {
+    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+    separator = "➜", -- symbol used between a key and it's label
+    group = "+", -- symbol prepended to a group
+  },
+  popup_mappings = {
+    scroll_down = "<c-d>", -- binding to scroll down inside the popup
+    scroll_up = "<c-u>", -- binding to scroll up inside the popup
+  },
+  window = {
+    border = "rounded", -- none, single, double, shadow
+    position = "bottom", -- bottom, top
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0,
+  },
+  layout = {
+    height = { min = 4, max = 25 }, -- min and max height of the columns
+    width = { min = 20, max = 50 }, -- min and max width of the columns
+    spacing = 3, -- spacing between columns
+    align = "left", -- align columns left, center or right
+  },
+  ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+  show_help = true, -- show help message on the command line when the popup is visible
+  triggers = "auto", -- automatically setup triggers
+  -- triggers = {"<leader>"} -- or specify a list manually
+  triggers_blacklist = {
+    -- list of mode / prefixes that should never be hooked by WhichKey
+    -- this is mostly relevant for key maps that start with a native binding
+    -- most people should not need to change this
+    i = { "j", "k" },
+    v = { "j", "k" },
+  },
+})
+
+menu.register({
+  ["<leader>r"] = { ":redo<cr>", "Redo" },
+  ["<leader>u"] = { ":undo<cr>", "Undo" },
+  ["<leader>e"] = { ":NvimTreeToggle<cr>", "Explorer" },
+  ["<leader>z"] = { ":Telescope find_files<cr>", "Filesystem" },
+  ["<leader>n"] = { ":noh<cr>", "No Highlighting" },
+  ["<leader>c"] = { ":bdelete<cr>", "Close Tab" },
+  ["<leader>;"] = { ":Dashboard<cr>", "Dashboard" },
+  ["<leader>T"] = { ":Tetris<cr>", "Tetris" },
+})
+
 menu.register({
   ["<Leader>"] = {
     d = {
-      name = "+Diagnostic",
-      d = {":TroubleToggle<CR>", "Show Diagnostic"},
+      name = "+Diagnostics",
+      d = {":TroubleToggle<cr>", "Show Diagnostics Menu"},
+      D = {":lua vim.lsp.buf.definition()<cr>", "Show Definition"},
+      i = {":lua vim.lsp.buf.implementation()<cr>", "Show Implementation"},
+      r = {":lua vim.lsp.buf.rename()<cr>", "Rename"},
+      I = {":lua vim.lsp.buf.hover()<cr>", "Show Informations"},
+      n = {":lua vim.lsp.diagnostic.goto_next()<cr>", "Show next Diagnostic"},
+      N = {":lua vim.lsp.diagnostic.goto_prev()<cr>", "Show previous Diagnostic"},
+      l = {":lua vim.lsp.diagnostic.show_line_diagnostics()<cr>", "Show Line Diagnostic"},
+      p = {":lua vim.lsp.diagnostic.show_position_diagnostics()<cr>", "Show Position Diagnostic"},
     }
   }
 })
 
 menu.register({
   ["<Leader>"] = {
-    m = {
-      name = "+Markdown",
-      o = {":MarkdownPreview<CR>", "Start Preview"},
-      c = {":MarkdownPreviewStop<CR>", "Stop Preview"},
-    }
-  }
-})
-
-menu.register({
-  ["<Leader>"] = {
-    e = {
-      name = "+Explorer", 
-      e = {":NvimTreeToggle<CR>", "Toggle Explorer"},
-      o = {":NvimTreeOpen<CR>", "Open Explorer"},
-      c = {":NvimTreeClose<CR>", "Close Explorer"},
-    }
-  }
-})
-
-menu.register({
-  ["<Leader>"] = {
-    b = {
-      name = "+Buffer (Tabs)", 
-      c = {":bdelete<CR>", "Close"},
-      d = {":BufferLinePickClose<CR>", "Close Selection"},
-      o = {":BufferLinePick<CR>", "GoTo Selection"},
-      n = {":BufferLineMoveNext<CR>", "Move Next"},
-      p = {":BufferLineMovePrev<CR>", "Move Prev"},
+    w = {
+      name = "+Formatting",
+      a = {":Autoformat<cr>", "Format File"},
+      b = {":AutoformatLine<cr>", "Format Line"},
+      f = {":RemoveTrailingSpaces<cr>", "Remove Trailing Spaces"},
     }
   }
 })
@@ -46,21 +108,34 @@ menu.register({
 menu.register({
   ["<Leader>"] = {
     f = {
-      name = "+Terminal", 
-      f = {":ToggleTerm direction=float<CR>", "Floating"},
-      h = {":ToggleTerm direction=horizontal<CR>", "Horizontal"},
+      name = "+Terminal",
+      t = {":ToggleTerm direction=float<cr>", "Floating Terminal"},
+      b = {":ToggleTerm<cr>", "Terminal on the bottom"},
     }
   }
 })
 
 menu.register({
   ["<Leader>"] = {
-    t = {
-      name = "+Telescope", 
-      f = {":Telescope find_files<CR>", "Files"},
-      l = {":Telescope live_grep<CR>", "Live Grep"},
-      t = {":Telescope buffers<CR>", "Open Editors"},
-      p = {":Telescope planets<CR>", "Planets"},
+    a = {
+      name = "+Lines",
+      b = {":call append(line('.'), '')<cr>", "Add Line Below"},
+      a = {":call append(line('.')-1, '')<cr>", "Add Line Above"},
+    }
+  }
+})
+
+menu.register({
+  ["<Leader>"] = {
+    b = {
+      name = "+File",
+      c = {":bdelete<cr>", "Close"},
+      d = {":BufferLinePickClose<cr>", "Close Selection"},
+      o = {":BufferLinePick<cr>", "GoTo Selection"},
+      n = {":BufferLineMoveNext<cr>", "Move Next"},
+      p = {":BufferLineMovePrev<cr>", "Move Prev"},
+      l = {":Telescope live_grep<cr>", "Live Grep"},
+      t = {":Telescope buffers<cr>", "Show open files"},
     }
   }
 })
@@ -68,11 +143,11 @@ menu.register({
 menu.register({
   ["<Leader>"] = {
     g = {
-      name = "+Git Actions", 
-      l = {":Gitsigns toggle_signs<CR>", "Toggle Git Changes Sign"},
-      s = {":Telescope git_status<CR>", "Git Status"},
-      c = {":Telescope git_commits<CR>", "Git Commits"},
-      b = {":Telescope git_branches<CR>", "Git Branches"},
+      name = "+Git",
+      l = {":Gitsigns toggle_signs<cr>", "Toggle Git Changes Sign"},
+      s = {":Telescope git_status<cr>", "Git Status"},
+      c = {":Telescope git_commits<cr>", "Git Commits"},
+      b = {":Telescope git_branches<cr>", "Git Branches"},
     }
   }
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,19 +1,34 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+  packer_bootstrap = fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
 return require("packer").startup(function()
   use "wbthomason/packer.nvim"
   use "navarasu/onedark.nvim"
+  use "alec-gibson/nvim-tetris"
   use "shaeinst/roshnivim-cs"
   use "akinsho/toggleterm.nvim"
+  use "tribela/vim-transparent"
   use "folke/which-key.nvim"
   use "terrortylor/nvim-comment"
-  use "hrsh7th/nvim-compe"
+  
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
+  use "L3MON4D3/LuaSnip"
+  use "rafamadriz/friendly-snippets"
+  use "saadparwaiz1/cmp_luasnip"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+
   use "windwp/nvim-autopairs"
-  use "glepnir/dashboard-nvim" 
+  use "glepnir/dashboard-nvim"
+  use "Chiel92/vim-autoformat"
   use { "neovim/nvim-lspconfig", "williamboman/nvim-lsp-installer" }
   use { "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons", }
   use { "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons", }
@@ -22,11 +37,36 @@ return require("packer").startup(function()
   use { "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } }
   use { "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } }
   use {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    ft = "markdown",
+    "nacro90/numb.nvim",
+    event = "BufRead",
     config = function()
-      vim.g.mkdp_auto_start = 1
+      require("numb").setup {
+        show_numbers = true,
+        show_cursorline = true,
+      }
     end,
   }
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate"
+  }
+  use {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+        '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+        hide_cursor = true,
+        stop_eof = true,
+        use_local_scrolloff = false,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing_function = nil,
+        pre_hook = nil,
+        post_hook = nil,
+      })
+    end
+  }
+
 end)
