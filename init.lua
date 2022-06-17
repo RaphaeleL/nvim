175261a1cd0cd6d3f60cb7e196cc920527611d0e
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-06-17 16:10:36 +0200

    clean init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
+-- Basic Neovim Settings
 require("plugins")
 require("settings")
 require("keymappings")
 require("autocommands")
 require("colors")
-
+-- Plugin Settings
 require("user.bufferline")
 require("user.nvim-gps")
 require("user.nvim-tree")
@@ -18,10 +19,6 @@ require("user.nvim_comment")
 require("user.indentline")
 require("user.nvim-autopairs")
 require("user.toggleterm")
---require("user.nvim-lsp-installer")
 require("user.lsp")
 require("user.lualine")
 require("user.cheatsheet")
-
-
-
