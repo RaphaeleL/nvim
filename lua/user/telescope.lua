fbbaa6ed48137be0810dc61560b3f8747defc4b8
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-06-18 17:42:05 +0200

    Updating README.md

---
+++ lua/user/telescope.lua
@@ -0,0 +1,66 @@
+local ok, actions = pcall(require, "telescope.actions")
+if not ok then
+	return
+end
+
+require("telescope").setup({
+	defaults = {
+		--prompt_prefix = " ",
+		--selection_caret = " ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "descending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			width = 0.75,
+			preview_cutoff = 120,
+			horizontal = {
+				preview_width = function(_, cols, _)
+					if cols < 120 then
+						return math.floor(cols * 0.5)
+					end
+					return math.floor(cols * 0.6)
+				end,
+				mirror = false,
+			},
+			vertical = { mirror = false },
+		},
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--hidden",
+			"--glob=!.git/",
+		},
+		file_ignore_patterns = {},
+		path_display = { shorten = 5 },
+		winblend = 0,
+		border = {},
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+		set_env = { ["COLORTERM"] = "truecolor" },
+		pickers = {
+			find_files = {
+				find_command = { "fd", "--type=file", "--hidden", "--smart-case" },
+			},
+			live_grep = {
+				--@usage don't include the filename in the search results
+				only_sort_text = true,
+			},
+		},
+	},
+	pickers = {},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
+})
