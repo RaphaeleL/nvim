4b2d98276640685536970ba411f4bb9ecd2f1473
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-06-18 10:20:42 +0200

    a bit of formatting and cleaning the file content

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -2,22 +2,22 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.header.val = {
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                     ",
+	"                                                     ",
+	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                     ",
 }
 
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <cr>"),
-    dashboard.button( "f", "  > Find file", ":Telescope find_files<cr>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<cr>"),
-    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<cr>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa!<cr>"),
+	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <cr>"),
+	dashboard.button("f", "  > Find file", ":Telescope find_files<cr>"),
+	dashboard.button("r", "  > Recent", ":Telescope oldfiles<cr>"),
+	dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<cr>"),
+	dashboard.button("q", "  > Quit NVIM", ":qa!<cr>"),
 }
 alpha.setup(dashboard.opts)
 vim.cmd([[
--- lua/user/nvim-treesitter.lua
+++ lua/user/nvim-treesitter.lua
@@ -1,6 +1,6 @@
-require'nvim-treesitter.configs'.setup {
-  highlight = {
+require("nvim-treesitter.configs").setup({
+	highlight = {
 
-    enable = true 
-  } 
-}
+		enable = true,
+	},
+})
