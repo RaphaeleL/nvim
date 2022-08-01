1c0634c3fcca7490c2187e8d91590b485170d443
raphaele <raphaele.salvatore@outlook.de>
2022-08-01 14:36:19 +0200

    jaq! finally

---
+++ lua/user/jaq.lua
@@ -0,0 +1,54 @@
+local status_ok, jaq= pcall(require, "jaq-nvim")
+if not status_ok then
+	require("user.notify").message("Coult not load jaq-nvim", "Loading Error", "error")
+	return
+end
+
+jaq.setup{
+  cmds = {
+    default = "term",
+    internal = {
+      lua = "luafile %",
+      vim = "source %"
+    },
+    external = {
+      javascript = "node %",
+      python   = "python3 %",
+      python3   = "python3 %",
+      go       = "go run %",
+      rust = "cargo run",
+      cpp = "g++ % -o $fileBase && ./$fileBase",
+    }
+  },
+
+  behavior = {
+    default     = "float",
+    startinsert = false,
+    wincmd      = false,
+    autosave    = false
+  },
+
+  ui = {
+    float = {
+      border    = "none",
+      winhl     = "Normal",
+      borderhl  = "FloatBorder",
+      winblend  = 0,
+      height    = 0.8,
+      width     = 0.8,
+      x         = 0.5,
+      y         = 0.5
+    },
+
+    terminal = {
+      position = "bot",
+      size     = 10,
+      line_no  = false
+    },
+
+    quickfix = {
+      position = "bot",
+      size     = 10
+    }
+  }
+}
