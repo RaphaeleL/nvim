2d262b56a548701a9eceebde83ab732b939fd885
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-08-01 14:41:55 +0200

    adding more langs to fucking jaq! :D

--- lua/user/jaq.lua
+++ lua/user/jaq.lua
@@ -1,54 +1,54 @@
-local status_ok, jaq= pcall(require, "jaq-nvim")
+local status_ok, jaq = pcall(require, "jaq-nvim")
 if not status_ok then
 	require("user.notify").message("Coult not load jaq-nvim", "Loading Error", "error")
 	return
 end
 
-jaq.setup{
-  cmds = {
-    default = "term",
-    internal = {
-      lua = "luafile %",
-      vim = "source %"
-    },
-    external = {
-      javascript = "node %",
-      python   = "python3 %",
-      python3   = "python3 %",
-      go       = "go run %",
-      rust = "cargo run",
-      cpp = "g++ % -o $fileBase && ./$fileBase",
-    }
-  },
+jaq.setup({
+	cmds = {
+		default = "term",
+		internal = {
+			lua = "luafile %",
+			vim = "source %",
+		},
+		external = {
+			javascript = "node %",
+			python = "python3 %",
+			go = "go run %",
+			rust = "cargo run",
+			cpp = "g++ % -o $fileBase -O3 && ./$fileBase",
+			c = "gcc % -o $fileBase -O3 && ./$fileBase",
+		},
+	},
 
-  behavior = {
-    default     = "float",
-    startinsert = false,
-    wincmd      = false,
-    autosave    = false
-  },
+	behavior = {
+		default = "float",
+		startinsert = false,
+		wincmd = false,
+		autosave = false,
+	},
 
-  ui = {
-    float = {
-      border    = "none",
-      winhl     = "Normal",
-      borderhl  = "FloatBorder",
-      winblend  = 0,
-      height    = 0.8,
-      width     = 0.8,
-      x         = 0.5,
-      y         = 0.5
-    },
+	ui = {
+		float = {
+			border = "none",
+			winhl = "Normal",
+			borderhl = "FloatBorder",
+			winblend = 0,
+			height = 0.8,
+			width = 0.8,
+			x = 0.5,
+			y = 0.5,
+		},
 
-    terminal = {
-      position = "bot",
-      size     = 10,
-      line_no  = false
-    },
+		terminal = {
+			position = "bot",
+			size = 10,
+			line_no = false,
+		},
 
-    quickfix = {
-      position = "bot",
-      size     = 10
-    }
-  }
-}
+		quickfix = {
+			position = "bot",
+			size = 10,
+		},
+	},
+})
