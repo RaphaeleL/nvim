d2e726044a2e84781036250178e1e7bd9f6a3e05
raphaele <raphaele.salvatore@outlook.de>
2022-08-10 11:29:47 +0200

    typo

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 require("plugins") -- Get all the Plugins (managed by Packer)
 require("settings") -- All (Neo)-Vim Settings
-require("keymappings") -- Basic Keymapps (more in Which-Key)
+require("keymappings") -- Basic Keymaps (more in Which-Key)
 require("autocommands") -- Commands which are executed automatically
 require("setup") -- Settings for every Plugin
