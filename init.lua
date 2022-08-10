9849af0930827c2fb103c1ae3da6e1ab8a8ce1c4
raphaele <raphaele.salvatore@outlook.de>
2022-08-10 11:28:16 +0200

    init strucuture improved

--- init.lua
+++ init.lua
@@ -1,27 +1,5 @@
--- Basic Neovim Settings
-require("plugins")
-require("settings")
-require("keymappings")
-require("autocommands")
-require("colors")
-
--- Plugin Settings
-require("user.bufferline")
--- require("user.nvim-gps")
-require("user.nvim-tree")
-require("user.which-key")
-require("user.gitsigns")
-require("user.cmp")
-require("user.alpha")
-require("user.nvim-treesitter")
-require("user.impatient")
-require("user.numb")
-require("user.nvim_comment")
-require("user.indentline")
-require("user.nvim-autopairs")
-require("user.toggleterm")
-require("user.lualine")
-require("user.telescope")
-require("user.notify")
-require("user.lsp")
-require("user.jaq")
+require("plugins") -- Get all the Plugins (managed by Packer)
+require("settings") -- All (Neo)-Vim Settings
+require("keymappings") -- Basic Keymapps (more in Which-Key)
+require("autocommands") -- Commands which are executed automatically
+require("setup") -- Settings for every Plugin
