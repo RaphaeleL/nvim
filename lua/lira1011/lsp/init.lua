c4c6841031e16fcf9830e7003c1a4e81ae28bf2d
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-02 14:50:22 +0200

    Remove Shade and cleanup LspInit

--- lua/lira1011/lsp/init.lua
+++ lua/lira1011/lsp/init.lua
@@ -1,8 +1,3 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
 require("lira1011.lsp.lsp-signature")
 require("lira1011.lsp.lsp-installer")
 require("lira1011.lsp.handlers").setup()
