81ed8e0080a888d00c8b77911bdce34bbd14e4d9
raphaele <raphaele.salvatore@outlook.de>
2022-09-02 13:44:44 +0200

    Better Descriptions

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 require("plugins") -- Get all the Plugins (managed by Packer)
 require("settings") -- All (Neo)-Vim Settings
-require("keymappings") -- Basic Keymaps (more in Which-Key)
+require("keymappings") -- Basic Keymaps (more are in lura/lira1011/which-key.lua)
 require("autocommands") -- Commands which are executed automatically
-require("setup") -- Settings for every Plugin
+require("setup") -- Settings for each Plugin
