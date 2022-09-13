f315a6736222dcad714db37d83e73a00ee99c670
raphaele <raphaele.salvatore@outlook.de>
2022-09-13 10:17:35 +0200

    Remove WhichKey

--- lua/lira1011/telescope.lua
+++ lua/lira1011/telescope.lua
@@ -65,20 +65,20 @@ telescope.setup({
 			override_file_sorter = true,
 			case_mode = "smart_case",
 		},
-		file_browser = {
-			theme = "ivy",
-			-- disables netrw and use telescope-file-browser in its place
-			hijack_netrw = true,
-			mappings = {
-				["i"] = {
-					-- your custom insert mode mappings
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-				},
-			},
-		},
+		-- file_browser = {
+		-- 	theme = "ivy",
+		-- 	-- disables netrw and use telescope-file-browser in its place
+		-- 	hijack_netrw = true,
+		-- 	mappings = {
+		-- 		["i"] = {
+		-- 			-- your custom insert mode mappings
+		-- 		},
+		-- 		["n"] = {
+		-- 			-- your custom normal mode mappings
+		-- 		},
+		-- 	},
+		-- },
 	},
 })
 
-require("telescope").load_extension("file_browser")
+-- require("telescope").load_extension("file_browser")
