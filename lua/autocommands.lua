e9d3f1767f76d340c01a66e8aab84f41bab9fd7a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-13 09:27:03 +0200

    Blazingy Fast with thePrimeagen

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -58,3 +58,15 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 -- 		vim.cmd([[source <afile> | PackerSync]])
 -- 	end,
 -- })
+
+-- Faster yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("HighlightYank", {}),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
