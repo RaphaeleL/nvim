8439a0f40a21a3c19eb99f4ffb75bc8bc31f5efc
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-14 09:59:33 +0200

    Install Packer

--- install.sh
+++ install.sh
@@ -13,9 +13,14 @@ git checkout release-0.7
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 
-# Remove the neovim Repo
+# Remove the Neovim Repo
 rm -rf ~/neovim
 
+# Get the Neovim Config
 git clone https://github.com/RaphaeleL/nvim.git ~/config/nvim
+
+# Install Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo "Type 'nvim' in the Terminal. Now you should be in Neovim :D yeey. Start install all the Plugins with 'PackerInstall'."
