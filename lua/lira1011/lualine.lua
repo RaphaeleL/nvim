f4621392afebb131ce547292c1760691750a3edf
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-21 10:50:46 +0200

    bring back the bubbles :D

--- lua/lira1011/lualine.lua
+++ lua/lira1011/lualine.lua
@@ -1,4 +1,4 @@
-local lualine = require("lualine")
+-- local lualine = require("lualine")
 
 local conditions = {
 	buffer_not_empty = function()
@@ -19,16 +19,127 @@ local function diff_source()
 		}
 	end
 end
-
+--
 -- vim.g.gitblame_display_virtual_text = 0
 -- local git_blame = require("gitblame")
+--
+-- lualine.setup({
+-- 	options = {
+-- 		theme = "kanagawa",
+-- 		component_separators = "|",
+-- 		section_separators = { left = "", right = "" },
+-- 	},
+-- 	sections = {
+-- 		lualine_a = {
+-- 			{
+-- 				function()
+-- 					return " "
+-- 				end,
+-- 				padding = { left = 0, right = 0 },
+-- 				color = {},
+-- 				cond = nil,
+-- 			},
+-- 		},
+-- 		lualine_b = {
+-- 			{
+-- 				"b:gitsigns_head",
+-- 				icon = " ",
+-- 				color = {},
+-- 				cond = conditions.hide_in_width,
+-- 			},
+-- 			{
+-- 				"filename",
+-- 				color = {},
+-- 				cond = nil,
+-- 			},
+-- 		},
+-- 		lualine_c = {
+-- 			-- { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+-- 		},
+-- 		lualine_x = {},
+-- 		lualine_y = {
+-- 			{
+-- 				"diff",
+-- 				source = diff_source,
+-- 				symbols = { added = "+", modified = "~", removed = "-" },
+-- 				diff_color = {
+-- 					added = { fg = "#98be65" },
+-- 					modified = { fg = "#ECBE7B" },
+-- 					removed = { fg = "#ec5f67" },
+-- 				},
+-- 				color = {},
+-- 				cond = nil,
+-- 			},
+-- 			{
+-- 				"diagnostics",
+-- 				sources = { "nvim_diagnostic" },
+-- 				symbols = { error = " ", warn = " ", info = " ", hint = " " },
+-- 				color = {},
+-- 				cond = conditions.hide_in_width,
+-- 			},
+-- 			{ "filetype", cond = conditions.hide_in_width, color = {} },
+-- 		},
+-- 		lualine_z = {
+-- 			{
+-- 				"location",
+-- 				padding = { left = 0, right = 0 },
+-- 				color = { fg = "#ECBE7B", bg = "#202328" },
+-- 				cond = nil,
+-- 			},
+-- 			{
+-- 				"progress",
+-- 				padding = { left = 0, right = 0 },
+-- 				color = { fg = "#98be65", bg = "#202328" },
+-- 				cond = nil,
+-- 			},
+-- 		},
+-- 	},
+-- 	inactive_sections = {
+-- 		lualine_a = {},
+-- 		lualine_b = {},
+-- 		lualine_c = { "filename" },
+-- 		lualine_x = { "getlocation" },
+-- 		lualine_y = {},
+-- 		lualine_z = {},
+-- 	},
+-- 	tabline = {},
+-- 	extensions = {},
+-- })
 
-lualine.setup({
-	options = {
-		theme = "kanagawa",
-		component_separators = "|",
-		section_separators = { left = "", right = "" },
-	},
+local colors = {
+  blue   = '#7e9cd8',
+  cyan   = '#79dac8',
+  black  = '#1f1f28',
+  white  = '#dcd7ba',
+  red    = '#c34043',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.black, bg = colors.black },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.black, bg = colors.black },
+  },
+}
+
+require('lualine').setup {
+  options = {
+    theme = bubbles_theme,
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
 	sections = {
 		lualine_a = {
 			{
@@ -102,6 +213,6 @@ lualine.setup({
 		lualine_y = {},
 		lualine_z = {},
 	},
-	tabline = {},
-	extensions = {},
-})
+  tabline = {},
+  extensions = {},
+}
