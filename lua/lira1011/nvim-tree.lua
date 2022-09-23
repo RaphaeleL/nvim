87da43072571a9eb90d28f4b10edb02bdf95c2de
raphaele <raphaele.salvatore@outlook.de>
2022-09-23 16:28:31 +0200

    Return of keymaps in nvimtree

--- lua/lira1011/nvim-tree.lua
+++ lua/lira1011/nvim-tree.lua
@@ -65,7 +65,12 @@ tree.setup({
 		signcolumn = "yes",
 		mappings = {
 			custom_only = false,
-			list = {},
+			list = {
+				{ key = "l", action = "edit", action_cb = edit_or_open },
+				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+				{ key = "h", action = "close_node" },
+				{ key = "H", action = "collapse_all", action_cb = collapse_all },
+			},
 		},
 	},
 	filters = {
