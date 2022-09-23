e1286d81658e5095a6409c6c1e8a7e1a731be7ad
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-23 11:28:19 +0200

    Interactive Install Script

--- install.sh
+++ install.sh
@@ -1,51 +1,119 @@
 #!/bin/bash
 
-echo "                                                            "
-echo "                          ⢠⣶⡤                               "
-echo "                         ⢠⣿⡿⠁                               "
-echo "                        ⢠⣿⡿⠁                      ⣀⣀        "
-echo "                       ⢀⣿⡿⠁                       ⣿⣿        "
-echo "     ⢀⣀⣀              ⢠⣿⡟                    ⢠⣶⣤⣀⡀⣿⣿ ⣀⣤⣴⣆   "
-echo "   ⢠⣾⡿⠛⠿⣿⣶⣄⣀⣴⣿⠟      ⢠⣿⡿                     ⠉⠉⠛⠻⢿⣿⣿⡿⠿⠛⠋⠉   "
-echo "   ⠉⠋    ⠙⠛⠿⠛⠁      ⢠⣿⡿⠁                        ⣴⣿⠟⠹⣿⣧⡀     "
-echo "                   ⣰⣿⡟⠁            ⣠⣶⣶⣦        ⠺⡿⠋  ⠈⢿⡷⠂    "
-echo "                  ⣰⣿⡟              ⢻⣿⣿⡿                     "
-echo "                 ⢠⣿⡟                ⠉⠉                      "
-echo "                  ⠉                                         "
-echo "                                                            "
-echo "                                                            "
-echo "            NaaVim - A Basic IDE Layor for Neovim           "
-echo "                                                            "
-echo "                                                            "
-echo "                                                            "
+function print_logo() {
+  clear
+  echo "                                                            "
+  echo "                          ⢠⣶⡤                               "
+  echo "                         ⢠⣿⡿⠁                               "
+  echo "                        ⢠⣿⡿⠁                      ⣀⣀        "
+  echo "                       ⢀⣿⡿⠁                       ⣿⣿        "
+  echo "     ⢀⣀⣀              ⢠⣿⡟                    ⢠⣶⣤⣀⡀⣿⣿ ⣀⣤⣴⣆   "
+  echo "   ⢠⣾⡿⠛⠿⣿⣶⣄⣀⣴⣿⠟      ⢠⣿⡿                     ⠉⠉⠛⠻⢿⣿⣿⡿⠿⠛⠋⠉   "
+  echo "   ⠉⠋    ⠙⠛⠿⠛⠁      ⢠⣿⡿⠁                        ⣴⣿⠟⠹⣿⣧⡀     "
+  echo "                   ⣰⣿⡟⠁            ⣠⣶⣶⣦        ⠺⡿⠋  ⠈⢿⡷⠂    "
+  echo "                  ⣰⣿⡟              ⢻⣿⣿⡿                     "
+  echo "                 ⢠⣿⡟                ⠉⠉                      "
+  echo "                  ⠉                                         "
+  echo "                                                            "
+  echo "                                                            "
+  echo "            NaaVim - A Basic IDE Layor for Neovim           "
+  echo "                                                            "
+  echo "                                                            "
+  echo "                                                            "
+}
 
-echo "Remove Previous Neovim ..."
-sudo rm -rf /usr/local/bin/nvim
-sudo rm -rf /usr/local/share/nvim/
+function remove_nvim() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Remove Previous Neovim ...                                 |"
+  echo "+-----------------------------------------------------------+"
+  rm -rf /usr/local/bin/nvim
+  rm -rf /usr/local/share/nvim/
+}
 
-echo "Remove Old Configuration ..."
-sudo rm -rf ~/.config/nvim
-sudo rm -rf ~/.local/bin/nvim
-sudo rm -rf ~/.local/share/nvim/
-sudo rm -rf ~/.cache/nvim
+function remove_config() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Remove Old Configuration ...                               |"
+  echo "+-----------------------------------------------------------+"
+  rm -rf ~/.config/nvim
+  rm -rf ~/.local/bin/nvim
+  rm -rf ~/.local/share/nvim/
+  rm -rf ~/.cache/nvim
+}
 
-echo "Clone the Neovim Repository ..."
-git clone https://github.com/neovim/neovim.git ~/neovim
+function clone_neovim() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Clone the Neovim Repository ...                            |"
+  echo "+-----------------------------------------------------------+"
+  git clone https://github.com/neovim/neovim.git ~/neovim
+}
 
-echo "Install Neovim ..."
-cd ~/neovim
-git checkout v0.7.2 
-make CMAKE_BUILD_TYPE=Release
-sudo make install
+function install_neovim() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Install Neovim ...                                         |"
+  echo "+-----------------------------------------------------------+"
+  cd ~/neovim
+  git checkout v0.7.2 
+  make CMAKE_BUILD_TYPE=Release
+  make install
+}
 
-echo "Remove Neovim Repository ..."
-rm -rf ~/neovim
+function remove_nvim_repo() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Remove Neovim Repository ...                               |"
+  echo "+-----------------------------------------------------------+"
+  rm -rf ~/neovim
+}
 
-echo "Clone the NaVim Configuration Repository ..."
-git clone https://github.com/RaphaeleL/nvim.git ~/config/nvim
+function clone_config() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Clone the NaVim Configuration Repository ...               |"
+  echo "+-----------------------------------------------------------+"
+  git clone https://github.com/RaphaeleL/nvim.git ~/config/nvim
+}
 
-echo "Install Packer ..."
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+function install_packer() {
+  echo "+-----------------------------------------------------------+"
+  echo "|Install Packer ...                                         |"
+  echo "+-----------------------------------------------------------+"
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+}
 
-echo "Make Sure to manually run ':PackerInstall' when starting nvim for the first time."
+function hint() {
+  print_logo 
+  echo "Make Sure to manually run ':PackerInstall' when starting nvim for the first time."
+}
+
+
+function confirm() {
+  local question="$1"
+  while true; do
+    msg "$question"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    case "$answer" in
+      y | Y | yes | YES | Yes)
+        return 0
+        ;;
+      n | N | no | NO | No | *[[:blank:]]* | "")
+        return 1
+        ;;
+      *)
+        msg "Please answer [y]es or [n]o."
+        ;;
+    esac
+  done
+}
+
+function main() {
+  print_logo 
+  if confirm "Would you like to install Neovim?"; then 
+    clone_neovim
+    install_neovim
+    remove_nvim_repo
+  fi
+  clone_config
+  install_packer
+  hint
+}
+
+main
