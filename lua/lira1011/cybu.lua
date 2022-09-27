b49477f515b01fa1a3c4d659095b3ff9bcb73ef9
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-27 11:06:18 +0200

    Cleanup cybu config

--- lua/lira1011/cybu.lua
+++ lua/lira1011/cybu.lua
@@ -1,19 +1,19 @@
 require("cybu").setup({
   position = {
     relative_to = "win",
-    anchor = "topright", 
+    anchor = "topright",
   },
   display_time = 1750,
   style = {
     path = "relative",
-    border = "rounded", 
+    border = "rounded",
     separator = " ",
     prefix = "…",
     padding = 1,
     hide_buffer_id = true,
     devicons = {
-      enabled = true, 
-      colored = true, 
+      enabled = true,
+      colored = true,
     },
   },
 })
