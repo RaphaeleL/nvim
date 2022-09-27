8f8df2fe025bd00b1acd010b3a997cce24d3c476
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-27 11:03:02 +0200

    clean cybu config

--- lua/lira1011/cybu.lua
+++ lua/lira1011/cybu.lua
@@ -1,26 +1,19 @@
 require("cybu").setup({
   position = {
-    relative_to = "win", -- win, editor, cursor
-    anchor = "topright", -- topleft, topcenter, topright,
-    -- centerleft, center, centerright,
-    -- bottomleft, bottomcenter, bottomright
-    -- vertical_offset = 10, -- vertical offset from anchor in lines
-    -- horizontal_offset = 0, -- vertical offset from anchor in columns
-    -- max_win_height = 5, -- height of cybu window in lines
-    -- max_win_width = 0.5, -- integer for absolute in columns
-    -- float for relative to win/editor width
+    relative_to = "win",
+    anchor = "topright", 
   },
-  display_time = 1750, -- time the cybu window is displayed
+  display_time = 1750,
   style = {
-    path = "relative", -- absolute, relative, tail (filename only)
-    border = "rounded", -- single, double, rounded, none
-    separator = " ", -- string used as separator
-    prefix = "…", -- string used as prefix for truncated paths
-    padding = 1, -- left & right padding in number of spaces
+    path = "relative",
+    border = "rounded", 
+    separator = " ",
+    prefix = "…",
+    padding = 1,
     hide_buffer_id = true,
     devicons = {
-      enabled = true, -- enable or disable web dev icons
-      colored = true, -- enable color for web dev icons
+      enabled = true, 
+      colored = true, 
     },
   },
 })
