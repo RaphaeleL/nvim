d7ab4763c4d48df53c3248f775b1e376409ae8ef
raphaele <raphaele.salvatore@outlook.de>
2022-09-29 18:14:45 +0200

    Adding Mason config

---
+++ lua/user/lsp/mason.lua
@@ -0,0 +1,72 @@
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+	return
+end
+
+local status_ok_1, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_ok_1 then
+	return
+end
+
+local servers = {
+	"cssls",
+	"cssmodules_ls",
+	"emmet_ls",
+	"html",
+	"jdtls",
+	"jsonls",
+	"solc",
+	"solidity_ls",
+	"tflint",
+	"terraformls",
+	"tsserver",
+	"pyright",
+	"yamlls",
+	"bashls",
+	"clangd",
+	"rust_analyzer",
+	"taplo",
+	"zk@v0.10.1",
+	"lemminx",
+}
+
+local settings = {
+	ui = {
+		border = "rounded",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
+}
+
+mason.setup(settings)
+mason_lspconfig.setup({
+	ensure_installed = servers,
+	automatic_installation = true,
+})
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+	server = vim.split(server, "@")[1]
+
+	lspconfig[server].setup(opts)
+	::continue::
+end
+
+-- TODO: add something to installer later
+-- require("lspconfig").motoko.setup {}
