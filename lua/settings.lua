dfe0bf7e6f0b47b89999f7acbd04a7b8165be40b
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-09-30 22:53:44 +0200

    nvim 8 !! :D

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,6 @@
 vim.opt.backup = false
 vim.opt.clipboard = "unnamedplus"
-vim.opt.cmdheight = 1
+vim.opt.cmdheight = 0
 vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.conceallevel = 0
 vim.opt.fileencoding = "utf-8"
