e26ac84b0c7b9053704571e001e1ff9490da8e4f
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-10-01 10:45:33 +0200

    Lsp Handlers updates

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -24,8 +24,22 @@ M.setup = function()
   end
 
   local config = {
+    -- disable virtual text
     virtual_lines = false,
     virtual_text = false,
+    -- virtual_text = {
+    --   -- spacing = 7,
+    --   -- update_in_insert = false,
+    --   -- severity_sort = true,
+    --   -- prefix = "<-",
+    --   prefix = " ●",
+    --   source = "if_many", -- Or "always"
+    --   -- format = function(diag)
+    --   --   return diag.message .. "blah"
+    --   -- end,
+    -- },
+
+    -- show signs
     signs = {
       active = signs,
     },
@@ -36,9 +50,11 @@ M.setup = function()
       focusable = true,
       style = "minimal",
       border = "rounded",
-      source = "if_many",
+      -- border = {"▄","▄","▄","█","▀","▀","▀","█"},
+      source = "if_many", -- Or "always"
       header = "",
       prefix = "",
+      -- width = 40,
     },
   }
 
@@ -46,13 +62,26 @@ M.setup = function()
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
+    -- width = 60,
+    -- height = 30,
   })
 
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     border = "rounded",
+    -- width = 60,
+    -- height = 30,
   })
 end
 
+local function lsp_highlight_document(client)
+  -- if client.server_capabilities.document_highlight then
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
+    return
+  end
+  illuminate.on_attach(client)
+  -- end
+end
 
 local function attach_navic(client, bufnr)
   vim.g.navic_silence = true
@@ -67,6 +96,7 @@ local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>Telescope lsp_declarations<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
@@ -74,17 +104,26 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<M-f>", "<cmd>Format<cr>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<M-a>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<M-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
+  lsp_highlight_document(client)
   attach_navic(client, bufnr)
 
   if client.name == "tsserver" then
-    require("lsp-inlayhints").on_attach(client, bufnr)
+    require("lsp-inlayhints").on_attach(bufnr, client)
   end
 
   if client.name == "jdt.ls" then
+    -- TODO: instantiate capabilities in java file later
     vim.lsp.codelens.refresh()
     if JAVA_DAP_ACTIVE then
       require("jdtls").setup_dap { hotcodereplace = "auto" }
@@ -97,7 +136,7 @@ function M.enable_format_on_save()
   vim.cmd [[
     augroup format_on_save
       autocmd! 
-      autocmd BufWritePre * lua vim.lsp.buf.format({ async = false }) 
+      autocmd BufWritePre * lua vim.lsp.buf.format({ async = true }) 
     augroup end
   ]]
   vim.notify "Enabled format on save"
