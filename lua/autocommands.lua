5e143ad4a5b547b2b092b51c8aad0860ed9b7078
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-10-11 10:12:21 +0200

    fix saving issue

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -47,11 +47,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 -- Autoformat on Save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	callback = function()
-		vim.lsp.buf.format({ async = true })
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+-- 	callback = function()
+-- 		vim.lsp.buf.format({ async = true })
+-- 	end,
+-- })
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
