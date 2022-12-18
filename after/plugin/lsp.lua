5621915f9188d98017999fb9e9afcefe17983c40
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-12-18 11:25:07 +0100

    Update LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,13 +5,31 @@ lsp.preset('recommended')
 vim.diagnostic.config({ virtual_text = true })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
+})
+
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+  'rust_analyzer',
+  'bashls',
+  'clangd',
+  'cmake',
+  'jsonls',
+  'ltex',
+  'sumneko_lua',
+  'marksman',
+  'pyright',
+  'rust_analyzer',
+  'yamlls',
 })
 
 lsp.setup()
