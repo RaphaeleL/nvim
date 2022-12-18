d6ecdb632b77b2506f4a82c7d7e51b03fcad3350
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-12-18 09:20:08 +0100

    Cleanup some Junk

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,7 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  use { "wbthomason/packer.nvim" }
+  use "wbthomason/packer.nvim"
 
   use "nvim-telescope/telescope-file-browser.nvim"
 
