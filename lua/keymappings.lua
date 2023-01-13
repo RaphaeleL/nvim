dde840735581cec4205c3d0e14f55bc7a148d779
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-13 10:14:53 +0100

    Better Navigation in Large Lines

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -38,3 +38,6 @@ vim.keymap.set("n", "<Leader>d", '"+d')
 vim.keymap.set("v", "<Leader>d", '"+d')
 vim.keymap.set("x", "<Leader>p", '"_dP')
 vim.keymap.set("n", "x", '"_x')
+
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
