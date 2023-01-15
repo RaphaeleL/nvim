85e0f269ef4b0eebaf9453737fa5df2c71f9709a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-15 09:36:43 +0100

    Remove TextObjects

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -44,22 +44,4 @@ treesitter.setup({
             "#c7aA6D",
         },
     },
-    -- textobjects = {
-    --     select = {
-    --         enable = true,
-    --         lookahead = true,
-    --         keymaps = {
-    --             ["af"] = "@function.outer",
-    --             ["if"] = "@function.inner",
-    --             ["ac"] = "@class.outer",
-    --             ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-    --         },
-    --         selection_modes = {
-    --             ['@parameter.outer'] = 'v',
-    --             ['@function.outer'] = 'V',
-    --             ['@class.outer'] = '<c-v>',
-    --         },
-    --         include_surrounding_whitespace = true,
-    --     },
-    -- },
 })
