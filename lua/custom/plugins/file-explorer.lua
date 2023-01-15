22ea6c0f28f77f13e2742ae8eb24063f1351bff1
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-15 10:12:50 +0100

    Add some Telescope Keymaps

--- lua/custom/plugins/file-explorer.lua
+++ lua/custom/plugins/file-explorer.lua
@@ -18,11 +18,15 @@ return {
             { "<leader>sf", ":Telescope find_files<cr>", desc = "Telescope: [S]how [F]uzzy File Finder" },
             { "<leader>sl", ":Telescope live_grep<cr>", desc = "Telescope: [S]how [L]ive Grep" },
             { "<leader>sg", ":Telescope git_files<cr>", desc = "Telescope: [S]how [G]it Files" },
-            { "<leader>sb", ":Telescope buffers<cr>", desc = "Telescope: [S]how [B]uffers" },
+            { "<leader><space>", ":Telescope buffers<cr>", desc = "Telescope: [S]how Buffers" },
             { "<leader>sd", ":Telescope diagnostics<cr>", desc = "Telescope: [S]how [D]iagnostics" },
             { "<leader>ss", ":Telescope grep_string<cr>", desc = "Telescope: [S]how [G]rep" },
             { "<leader>so", ":Telescope lsp_document_symbols<cr>", desc = "Telescope: [S]how [O]utliner" },
             { "<leader>sk", ":Telescope keymaps<cr>", desc = "Telescope: [S]how [K]eymaps" },
+            { "<leader>/",
+                ":lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, })<cr>",
+                desc = "Telescope: Search in the Current File" },
+            { "<leader>?", ":Telescope oldfiles<cr>", desc = "Telescope: [S]how Oldfiles" },
         }
     },
 }
