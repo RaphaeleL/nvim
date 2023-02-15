49b29325cce6817f7e4b4e7dfbf87c785bdc7afa
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-02-15 13:20:52 +0100

    Declutter & Auto Dark Light Mode Colorscheme (Option)

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,4 +1,18 @@
 return {
+    -- ========================================
+    --      My Own Colorscheme
+    -- ========================================
+    {
+        "RaphaeleL/my_vivid",
+        config = function()
+            vim.cmd("colorscheme my_vivid")
+            vim.cmd("hi @field guifg=#89A5AC")
+            vim.cmd("hi @parameter guifg=#77B0DA")
+        end
+    },
+    -- ========================================
+    --      Some cool basic Colorschemes 
+    -- ========================================
     -- {
     --     "rebelot/kanagawa.nvim",
     --     event = "VeryLazy",
@@ -24,19 +38,42 @@ return {
     --         vim.cmd("colorscheme catppuccin")
     --     end
     -- },
-    {
-        "RaphaeleL/my_vivid",
-        config = function()
-            vim.cmd("colorscheme my_vivid")
-            vim.cmd("hi @field guifg=#89A5AC")
-            vim.cmd("hi @parameter guifg=#77B0DA")
-        end
-    },
+    -- ========================================
+    --      Auto Dark / Light Colorscheme 
+    -- ========================================
     -- {
-    --     dir = "~/Developer/vscode.nvim/",
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
     --     event = "VeryLazy",
     --     config = function()
-    --         vim.cmd("colorscheme vscode")
+    --     require("catppuccin").setup({
+    --         flavour = "mocha",
+    --         background = {
+    --             light = "latte",
+    --             dark = "mocha",
+    --         },
+    --         transparent_background = false,
+    --     })
+    --     vim.cmd("colorscheme catppuccin")
+    --     end
+    -- },
+    -- {
+    --     "f-person/auto-dark-mode.nvim",
+    --     config = function()
+    --         local auto_dark_mode = require('auto-dark-mode')
+    --
+    --         auto_dark_mode.setup({
+    --             update_interval = 1000,
+    --             set_dark_mode = function()
+    --                 vim.api.nvim_set_option('background', 'dark')
+    --                 vim.cmd('colorscheme gruvbox')
+    --             end,
+    --             set_light_mode = function()
+    --                 vim.api.nvim_set_option('background', 'light')
+    --                 vim.cmd('colorscheme gruvbox')
+    --             end,
+    --         })
+    --         auto_dark_mode.init()
     --     end
     -- },
 }
