08c345de4d881703661f80cadb10ea37c89fa274
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-02-16 08:23:51 +0100

    Spectre, WhichKey

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -16,4 +16,23 @@ return {
             require("nvim-autopairs").setup()
         end,
     },
+    {
+        "windwp/nvim-spectre",
+        lazy = true,
+        keys = {
+            { "<leader>S", ":lua require('spectre').open()<cr>" },
+        }
+    },
+    -- {
+    --     "folke/which-key.nvim",
+    --     lazy = true,
+    --     keys = {
+    --         { "<leader><leader>", ":WhichKey<cr>", desc = "(NaaVim) Open WhichKey" },
+    --     },
+    --     config = function()
+    --         vim.o.timeout = true
+    --         vim.o.timeoutlen = 300
+    --         require("which-key").setup({ })
+    --     end,
+    -- },
 }
