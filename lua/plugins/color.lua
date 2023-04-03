f424782968a4fa26bd8e56fbcade95fc0ebbeb1d
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-04-03 10:57:26 +0200

    Kanagawa

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -13,19 +13,37 @@ return {
     -- ========================================
     --      Some cool basic Colorschemes
     -- ========================================
-    -- {
-    --     "rebelot/kanagawa.nvim",
-    --     config = function()
-    --         require("kanagawa").setup({ 
-    --             transparent = true,
-    --             background = {
-    --                 dark = "wave", 
-    --                 light = "lotus"
-    --             }
-    --         })
-    --         vim.cmd("colorscheme kanagawa")
-    --     end
-    -- },
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                transparent = true,
+                overrides = function(colors)
+                    return {
+                        StatusLine = { bg = "NONE" },
+                        StatusLineNC = { bg = "NONE" },
+                        TabLineSel = { bg = "NONE" },
+                        LineNr = { bg = "NONE" },
+                        CursorLineNr = { bg = "NONE" },
+                        SignColumn = { bg = "NONE" },
+                        GitSignsAdd = { bg = "NONE" },
+                        GitSignsChange = { bg = "NONE" },
+                        GitSignsDelete = { bg = "NONE" },
+                        DiagnosticSignError = { bg = "NONE" },
+                        DiagnosticSignWarn = { bg = "NONE" },
+                        DiagnosticSignInfo = { bg = "NONE" },
+                        DiagnosticSignHint = { bg = "NONE" },
+                        TelescopeBorder = { bg = "NONE" },
+                    }
+                end,
+                background = {
+                    dark = "dragon",
+                    light = "lotus"
+                }
+            })
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
     -- {
     --     "rose-pine/neovim",
     --     config = function()
@@ -33,23 +51,23 @@ return {
     --         vim.cmd("colorscheme rose-pine")
     --     end
     -- },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-            require("catppuccin").setup({ 
-                background = {
-                    light = "latte",
-                    dark = "mocha",
-                },
-                transparent_background = true,
-                integrations = {
-                    illuminate = true
-                }
-            })
-            vim.cmd("colorscheme catppuccin")
-        end
-    },
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     config = function()
+    --         require("catppuccin").setup({
+    --             background = {
+    --                 light = "latte",
+    --                 dark = "mocha",
+    --             },
+    --             transparent_background = true,
+    --             integrations = {
+    --                 illuminate = true
+    --             }
+    --         })
+    --         vim.cmd("colorscheme catppuccin")
+    --     end
+    -- },
     -- ========================================
     --      Auto Dark / Light Colorscheme
     -- ========================================
@@ -90,4 +108,16 @@ return {
     --         auto_dark_mode.init()
     --     end
     -- },
+    -- ========================================
+    --      TJ Devries Colorscheme
+    -- ========================================
+    -- {
+    --     "tjdevries/colorbuddy.nvim",
+    -- },
+    -- {
+    --     "tjdevries/gruvbuddy.nvim",
+    --     config = function()
+    --         require('colorbuddy').colorscheme('gruvbuddy')
+    --     end
+    -- }
 }
