791e59fb2feac2a9188e7d431f27d520fcd5f80c
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-04-06 10:29:02 +0200

    Breadcrumps, Neoscroll, Whichkey

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -64,5 +64,51 @@ return {
     --         require("illuminate")
     --     end,
     -- },
-    { 'folke/which-key.nvim', opts = {} },
+    {
+        'folke/which-key.nvim',
+        opts = {}
+    },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {}
+    },
+    -- {
+    --     "fgheng/winbar.nvim",
+    --     dependencies = {
+    --         "SmiteshP/nvim-gps",
+    --         "SmiteshP/nvim-navic",
+    --     },
+    --     config = function()
+    --         require("nvim-gps").setup()
+    --         require('winbar').setup({
+    --             enabled = true,
+    --             show_file_path = true,
+    --             show_symbols = true,
+    --             colors = {
+    --                 path = '',
+    --                 file_name = '',
+    --                 symbols = '',
+    --             },
+    --             highlight = true,
+    --             separator = ">",
+    --             depth_limit = 0,
+    --             depth_limit_indicator = "..",
+    --             exclude_filetype = {
+    --                 'help',
+    --                 'startify',
+    --                 'dashboard',
+    --                 'packer',
+    --                 'neogitstatus',
+    --                 'NvimTree',
+    --                 'Trouble',
+    --                 'alpha',
+    --                 'lir',
+    --                 'Outline',
+    --                 'spectre_panel',
+    --                 'toggleterm',
+    --                 'qf',
+    --             }
+    --         })
+    --     end
+    -- }
 }
