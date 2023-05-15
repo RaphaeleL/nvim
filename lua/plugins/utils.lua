1650e6afc88069a37ca58e7c009a357673c810ed
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-05-15 08:01:24 +0200

    Better Highlight for "f-Movement"

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -49,6 +49,8 @@ return {
                 highlight_on_key = true,
                 dim = false
             }
+            vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg='#ff00ff', bold = true, underline = false })
+            vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#00ffff', underline = false })
         end
     },
 }
