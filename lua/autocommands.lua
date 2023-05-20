2cc4a2a5c2db2f2ab2e21210d166c3d82cc24dc6
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-05-20 15:08:25 +0200

    Some (disabled) LSP Autocmds

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -17,3 +17,43 @@ vim.cmd [[
     autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'cd '.argv()[0] | exe 'NvimTreeOpen' | wincmd p | ene | exe 'cd '.getcwd() | endif
   augroup END
 ]]
+
+-- Show line diagnostics automatically in hover window
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+-- Highlight line number instead of having icons in sign column
+-- vim.cmd [[
+--   highlight! DiagnosticLineNrError guibg=#51202A guifg=#FF0000 gui=bold
+--   highlight! DiagnosticLineNrWarn guibg=#51412A guifg=#FFA500 gui=bold
+--   highlight! DiagnosticLineNrInfo guibg=#1E535D guifg=#00FFFF gui=bold
+--   highlight! DiagnosticLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
+--
+--   sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
+--   sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
+--   sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
+--   sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
+-- ]]
+
+-- Highlight symbol under cursor (aka selfmade Illuminate)
+-- vim.cmd [[
+--     hi! LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+--     hi! LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+--     hi! LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+-- ]]
+-- vim.api.nvim_create_augroup('lsp_document_highlight', {
+--     clear = false
+--   })
+-- vim.api.nvim_clear_autocmds({
+--     buffer = bufnr,
+--     group = 'lsp_document_highlight',
+-- })
+-- vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+--     group = 'lsp_document_highlight',
+--     buffer = bufnr,
+--     callback = vim.lsp.buf.document_highlight,
+-- })
+-- vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+--     group = 'lsp_document_highlight',
+--     buffer = bufnr,
+--     callback = vim.lsp.buf.clear_references,
+-- })
