5d4bc7f2954fa4661b84dc23797f152f1489a6e8
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-05-20 15:32:23 +0200

    Cooler Telescope Theme :D

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -3,16 +3,16 @@ return {
         "nvim-telescope/telescope.nvim",
         lazy = true,
         keys = {
-            { "<leader>sf", ":Telescope find_files theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [F]uzzy File Finder" },
-            { "<leader>sl", ":Telescope live_grep theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [L]ive Grep" },
-            { "<leader>sg", ":Telescope git_files theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [G]it Files" },
-            { "<leader>sb", ":Telescope buffers theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how Buffers" },
-            { "<leader>sd", ":Telescope diagnostics theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [D]iagnostics" },
-            { "<leader>ss", ":Telescope grep_string theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [G]rep" },
-            { "<leader>so", ":Telescope lsp_document_symbols theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [O]utliner" },
-            { "<leader>sk", ":Telescope keymaps theme=dropdown<cr>", desc = "(NaaVim) Telescope: [S]how [K]eymaps" },
-            { "<leader>?", ":Telescope oldfiles theme=dropdown<cr>", desc = "(NaaVim) Telescope: Show Oldfiles" },
-            { "<leader>/", ":lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, })<cr>", desc = "(NaaVim) Telescope: Search in the Current File" },
+            { "<leader>sf", ":Telescope find_files theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [F]uzzy File Finder" },
+            { "<leader>sl", ":Telescope live_grep theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [L]ive Grep" },
+            { "<leader>sg", ":Telescope git_files theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [G]it Files" },
+            { "<leader>sb", ":Telescope buffers theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how Buffers" },
+            { "<leader>sd", ":Telescope diagnostics theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [D]iagnostics" },
+            { "<leader>ss", ":Telescope grep_string theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [G]rep" },
+            { "<leader>so", ":Telescope lsp_document_symbols theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [O]utliner" },
+            { "<leader>sk", ":Telescope keymaps theme=ivy<cr>", desc = "(NaaVim) Telescope: [S]how [K]eymaps" },
+            { "<leader>?", ":Telescope oldfiles theme=ivy<cr>", desc = "(NaaVim) Telescope: Show Oldfiles" },
+            { "<leader>/", ":lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_ivy { winblend = 10, previewer = false, })<cr>", desc = "(NaaVim) Telescope: Search in the Current File" },
         },
         config = function()
             require('telescope').setup({
@@ -22,6 +22,11 @@ return {
                         ".DS_Store"
                     }
                 },
+                pickers = {
+                    find_files = {
+                        theme = "ivy",
+                    }
+              },
             })
         end,
     },
