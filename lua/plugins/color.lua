92b3dfafbcaff10dd432fc7cbc9c04e5b3b1f5fa
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-06-26 11:07:44 +0200

    Try out some Colorschemes

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -45,6 +45,23 @@ return {
 		end
 	},
     -- {
+    --     "ramojus/mellifluous.nvim",
+    --     config = function()
+    --         require("mellifluous").setup({
+    --             transparent_background = {
+    --                 enabled = true,
+    --             }
+    --         })
+    --         vim.cmd("colorscheme mellifluous")
+    --     end,
+    -- },
+    -- {
+    --     "LunarVim/primer.nvim",
+    --     config = function()
+    --         vim.cmd("colorscheme primer_dark")
+    --     end,
+    -- },
+    -- {
     --     "LunarVim/darkplus.nvim",
     --     config = function()
     --         vim.cmd("colorscheme darkplus")
