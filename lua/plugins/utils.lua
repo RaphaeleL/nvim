fd0938cbf53ffba31b767485fee931620881c573
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-07-08 09:46:58 +0200

    not using you.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -54,19 +54,4 @@ return {
             vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#00ffff', underline = false })
         end
     },
-    {
-        "ThePrimeagen/harpoon",
-        lazy = true,
-        keys = {
-            { "mm", ":lua require('harpoon.mark').add_file()<cr>", desc = "(NaaVim) [M]ark a File" },
-            { "mn", ":lua require('harpoon.ui').nav_next()<cr>", desc = "(NaaVim) [M]ove to [n]ext File" },
-            { "mp", ":lua require('harpoon.ui').nav_prev()<cr>", desc = "(NaaVim) [M]ove to [p]revious File" },
-            { "mc", ":lua require('harpoon.mark').clear_all()<cr>", desc = "(NaaVim) [C]lear all Marks" },
-            { "mr", ":lua require('harpoon.mark').rm_file()<cr>", desc = "(NaaVim) [R]emove Selected Mark" },
-            { "ma", ":lua require('harpoon.ui').nav_file(1)<cr>", desc = "(NaaVim) [M]ove to 1" },
-            { "ms", ":lua require('harpoon.ui').nav_file(2)<cr>", desc = "(NaaVim) [M]ove to 2" },
-            { "md", ":lua require('harpoon.ui').nav_file(3)<cr>", desc = "(NaaVim) [M]ove to 3" },
-            { "mf", ":lua require('harpoon.ui').nav_file(4)<cr>", desc = "(NaaVim) [M]ove to 4" },
-        }
-    }
 }
