d35521e2f21b254122d40eddfe4415838f7590dc
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-11-16 06:15:30 +0100

    use catppuccin

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -25,45 +25,45 @@ return {
 	-- ========================================
 	--      Some cool basic Colorschemes
 	-- ========================================
-	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("kanagawa").setup({
-				transparent = true,
-				overrides = function(colors)
-					return {
-						StatusLine = { bg = "NONE" },
-						StatusLineNC = { bg = "NONE" },
-						TabLineSel = { bg = "NONE" },
-						LineNr = { bg = "NONE" },
-						CursorLineNr = { bg = "NONE" },
-						SignColumn = { bg = "NONE" },
-						GitSignsAdd = { bg = "NONE" },
-						GitSignsChange = { bg = "NONE" },
-						GitSignsDelete = { bg = "NONE" },
-						DiagnosticSignError = { bg = "NONE" },
-						DiagnosticSignWarn = { bg = "NONE" },
-						DiagnosticSignInfo = { bg = "NONE" },
-						DiagnosticSignHint = { bg = "NONE" },
-						TelescopeBorder = { bg = "NONE" },
-                        
-                        TelescopeTitle = { fg = colors.theme.ui.special, bold = true },
-                        TelescopePromptNormal = { bg = colors.theme.ui.bg_p1 },
-                        TelescopePromptBorder = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
-                        TelescopeResultsNormal = { fg = colors.theme.ui.fg_dim, bg = colors.theme.ui.bg_m1 },
-                        TelescopeResultsBorder = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
-                        TelescopePreviewNormal = { bg = colors.theme.ui.bg_dim },
-                        TelescopePreviewBorder = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
-					}
-				end,
-				background = {
-					dark = "dragon",
-					light = "lotus"
-				}
-			})
-			vim.cmd("colorscheme kanagawa")
-		end
-	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	config = function()
+	-- 		require("kanagawa").setup({
+	-- 			transparent = true,
+	-- 			overrides = function(colors)
+	-- 				return {
+	-- 					StatusLine = { bg = "NONE" },
+	-- 					StatusLineNC = { bg = "NONE" },
+	-- 					TabLineSel = { bg = "NONE" },
+	-- 					LineNr = { bg = "NONE" },
+	-- 					CursorLineNr = { bg = "NONE" },
+	-- 					SignColumn = { bg = "NONE" },
+	-- 					GitSignsAdd = { bg = "NONE" },
+	-- 					GitSignsChange = { bg = "NONE" },
+	-- 					GitSignsDelete = { bg = "NONE" },
+	-- 					DiagnosticSignError = { bg = "NONE" },
+	-- 					DiagnosticSignWarn = { bg = "NONE" },
+	-- 					DiagnosticSignInfo = { bg = "NONE" },
+	-- 					DiagnosticSignHint = { bg = "NONE" },
+	-- 					TelescopeBorder = { bg = "NONE" },
+	--                        
+	--                         TelescopeTitle = { fg = colors.theme.ui.special, bold = true },
+	--                         TelescopePromptNormal = { bg = colors.theme.ui.bg_p1 },
+	--                         TelescopePromptBorder = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
+	--                         TelescopeResultsNormal = { fg = colors.theme.ui.fg_dim, bg = colors.theme.ui.bg_m1 },
+	--                         TelescopeResultsBorder = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
+	--                         TelescopePreviewNormal = { bg = colors.theme.ui.bg_dim },
+	--                         TelescopePreviewBorder = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
+	-- 				}
+	-- 			end,
+	-- 			background = {
+	-- 				dark = "dragon",
+	-- 				light = "lotus"
+	-- 			}
+	-- 		})
+	-- 		vim.cmd("colorscheme kanagawa")
+	-- 	end
+	-- },
 	-- {
 	--     "rose-pine/neovim",
 	--     config = function()
@@ -71,21 +71,21 @@ return {
 	--         vim.cmd("colorscheme rose-pine")
 	--     end
 	-- },
-	-- {
-	--     "catppuccin/nvim",
-	--     name = "catppuccin",
-	--     config = function()
-	--         require("catppuccin").setup({
-	--             background = {
-	--                 light = "latte",
-	--                 dark = "mocha",
-	--             },
-	--             transparent_background = true,
-	--             integrations = {
-	--                 illuminate = true
-	--             }
-	--         })
-	--         vim.cmd("colorscheme catppuccin")
-	--     end
-	-- },
+	{
+	    "catppuccin/nvim",
+	    name = "catppuccin",
+	    config = function()
+	        require("catppuccin").setup({
+	            background = {
+	                light = "latte",
+	                dark = "mocha",
+	            },
+	            transparent_background = true,
+	            integrations = {
+	                illuminate = true
+	            }
+	        })
+	        vim.cmd("colorscheme catppuccin")
+	    end
+	},
 }
