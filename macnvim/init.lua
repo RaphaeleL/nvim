59f629c624d38c6a284b223c5713bf50421a5741
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-12-03 12:41:00 +0100

    Update Custom VI

--- macnvim/init.lua
+++ macnvim/init.lua
@@ -32,59 +32,62 @@ vim.opt.modifiable = true
 -------------
 -- Keymaps --
 -------------
-vim.g.mapleader = " "
-
 -- Buffer Manipulation
-vim.keymap.set("i", "kj", "<ESC>")
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("n", "<Space>", "<NOP>")
-vim.keymap.set("n", "<C-s>", ":wa!<cr>")
-vim.keymap.set("n", "<C-w>", ":wqa!<cr>")
-vim.keymap.set("n", "<TAB>", ":write<cr>:bprev<cr>")
-vim.keymap.set("n", "<leader>bn", ":write<cr>:bprev<cr>")
-vim.keymap.set("n", "<S-TAB>", ":write<cr>:bnext<cr>")
-vim.keymap.set("n", "<leader>bp", ":write<cr>:bnext<cr>")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<leader>bv", ":vsplit<cr>")
-vim.keymap.set("n", "<leader>bh", ":split<cr>")
-vim.keymap.set("n", "<leader>bq", ":bdelete!<cr>")
-vim.keymap.set("n", "c", ":close!<cr>")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<Leader>n", ":noh<cr>")
-vim.keymap.set("n", "<S-Up>", ":resize -2<CR>")
-vim.keymap.set("n", "<S-Down>", ":resize +2<CR>")
-vim.keymap.set("n", "<S-Left>", ":vertical resize -2<CR>")
-vim.keymap.set("n", "<S-Right>", ":vertical resize +2<CR>")
+map("i", "kj", "<ESC>")
+map("i", "jk", "<ESC>")
+map("n", "<Space>", "<NOP>")
+map("n", "q", ":q!<cr>")
+map("n", "<TAB>", ":write<cr>:bprev<cr>")
+map("n", "<S-TAB>", ":write<cr>:bnext<cr>")
+
+map("n", "sh", "<C-w>h")
+map("n", "sj", "<C-w>j")
+map("n", "sk", "<C-w>k")
+map("n", "sl", "<C-w>l")
+map("n", "sv", ":vsplit<cr>")
+map("n", "ss", ":split<cr>")
+
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+map("n", "<Leader>n", ":noh<cr>")
+
+map("n", "<S-Up>", ":resize -2<CR>")
+map("n", "<S-Down>", ":resize +2<CR>")
+map("n", "<S-Left>", ":vertical resize -2<CR>")
+map("n", "<S-Right>", ":vertical resize +2<CR>")
 
 -- Char Manipulation
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>c", ":! make -j4")
-vim.keymap.set("n", "+", "<C-a>")
-vim.keymap.set("n", "-", "<C-x>")
-vim.keymap.set("n", "<Leader>Y", '"+Y')
-vim.keymap.set("x", "<Leader>p", '"_dP')
-vim.keymap.set("n", "x", '"_x')
-vim.keymap.set("i", "<C-BS>", "<Esc>cvb")
+map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
+
+map("n", "<Leader>Y", '"+Y')
+map("x", "<Leader>p", '"_dP')
+map("n", "x", '"_x')
+map("i", "<C-BS>", "<Esc>cvb")
 
 -- Line Manipulation
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'")
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'")
-vim.keymap.set({ "n", "v" }, "<Leader>l", ":CommentToggle<cr>")
-vim.keymap.set("n", "<leader>ba", "GVgg")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "H", "0")
-vim.keymap.set("n", "L", "$")
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = " Line: Move in a Linebreak up" })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true, desc = " Line: Move in a Linebreak down" })
+
+map({ "n", "v" }, "<Leader>l", ":CommentToggle<cr>")
+
+map("n", "<C-a>", "GVgg")
+map("n", "J", "mzJ`z")
+
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "H", "0")
+map("n", "L", "$")
 
 -- File System 
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
+map("n", "<leader>e", ":NvimTreeToggle<cr>")
 
 --------------------
 -- Plugin Manager --
