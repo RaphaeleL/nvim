92f21d4a31ec89396e3079a77b21f1b2f921e272
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-04 06:35:30 +0100

    More Colorschemes

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,25 +1,49 @@
 return {
+    -- {
+    --     "rose-pine/neovim",
+    --     config = function()
+    --         require("rose-pine").setup({ disable_background = true })
+    --         -- vim.cmd("colorscheme rose-pine")
+    --     end
+    -- },
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     config = function()
+    --         require("catppuccin").setup({
+    --             background = {
+    --                 light = "latte",
+    --                 dark = "mocha",
+    --             },
+    --             transparent_background = true,
+    --             integrations = {
+    --                 illuminate = true
+    --             }
+    --         })
+    --         -- vim.cmd("colorscheme catppuccin")
+    --     end
+    -- },
     {
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("kanagawa").setup({
-				transparent = true,
-				overrides = function(colors)
-					return {
-						StatusLine = { bg = "NONE" },
-						StatusLineNC = { bg = "NONE" },
-						TabLineSel = { bg = "NONE" },
-						LineNr = { bg = "NONE" },
-						CursorLineNr = { bg = "NONE" },
-						SignColumn = { bg = "NONE" },
-						GitSignsAdd = { bg = "NONE" },
-						GitSignsChange = { bg = "NONE" },
-						GitSignsDelete = { bg = "NONE" },
-						DiagnosticSignError = { bg = "NONE" },
-						DiagnosticSignWarn = { bg = "NONE" },
-						DiagnosticSignInfo = { bg = "NONE" },
-						DiagnosticSignHint = { bg = "NONE" },
-						TelescopeBorder = { bg = "NONE" },
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                transparent = true,
+                overrides = function(colors)
+                    return {
+                        StatusLine = { bg = "NONE" },
+                        StatusLineNC = { bg = "NONE" },
+                        TabLineSel = { bg = "NONE" },
+                        LineNr = { bg = "NONE" },
+                        CursorLineNr = { bg = "NONE" },
+                        SignColumn = { bg = "NONE" },
+                        GitSignsAdd = { bg = "NONE" },
+                        GitSignsChange = { bg = "NONE" },
+                        GitSignsDelete = { bg = "NONE" },
+                        DiagnosticSignError = { bg = "NONE" },
+                        DiagnosticSignWarn = { bg = "NONE" },
+                        DiagnosticSignInfo = { bg = "NONE" },
+                        DiagnosticSignHint = { bg = "NONE" },
+                        TelescopeBorder = { bg = "NONE" },
                         TelescopeTitle = { fg = colors.theme.ui.special, bold = true },
                         TelescopePromptNormal = { bg = colors.theme.ui.bg_p1 },
                         TelescopePromptBorder = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
@@ -27,14 +51,14 @@ return {
                         TelescopeResultsBorder = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
                         TelescopePreviewNormal = { bg = colors.theme.ui.bg_dim },
                         TelescopePreviewBorder = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
-					}
-				end,
-				background = {
-					dark = "dragon",
-					light = "lotus"
-				}
-			})
-			vim.cmd("colorscheme kanagawa")
-		end
-	},
+                    }
+                end,
+                background = {
+                    dark = "dragon",
+                    light = "lotus"
+                }
+            })
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
 }
