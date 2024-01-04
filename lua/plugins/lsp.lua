013467ccd4974c524c2f87d117298ad1e8714f73
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-04 08:50:51 +0100

    Lsp-Zero v3

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,69 +1,43 @@
 return {
-   {
-        "neovim/nvim-lspconfig",
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
         dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "folke/neodev.nvim",
-            "hrsh7th/nvim-cmp",
+            "neovim/nvim-lspconfig",
             "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/nvim-cmp",
             "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim"
         },
         config = function()
-            local on_attach = function(_, bufnr)
-                local nmap = function(keys, func, desc)
-                    if desc then desc = " LSP: " .. desc end
-                    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-                end
-                nmap("ca", ":lua vim.lsp.buf.code_action()<cr>", "[C]ode [A]ction")
-                nmap("sd", ":lua vim.diagnostic.open_float()<cr>", "[O]pen [D]iagnostics")
-                nmap("gr", ":lua vim.lsp.buf.references()<cr>", "[G]oto [R]eference")
-                nmap("gd", ":lua vim.lsp.buf.definition()<cr>", "[G]oto [D]efinition")
-                nmap("gn", ":lua vim.diagnostic.goto_next()<cr>zz", "[G]oto [N]ext Diagnostic")
-                nmap("gp", ":lua vim.diagnostic.goto_prev()<cr>zz", "[G]oto [P]revious Diagnostic")
-                nmap("K", ":lua vim.lsp.buf.hover()<cr>", "Hover Documentation")
-                nmap("FF", ":lua vim.lsp.buf.format()<cr>", "[F]ormat [F]ile")
-            end
+            local lsp_zero = require("lsp-zero")
 
-            local servers = { }
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
 
-            require("neodev").setup()
-
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-            require("mason").setup()
-
-            local mason_lspconfig = require "mason-lspconfig"
-
-            mason_lspconfig.setup {
-                ensure_installed = vim.tbl_keys(servers),
-            }
-
-            mason_lspconfig.setup_handlers {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                        settings = servers[server_name],
-                    }
-                end,
-
-            }
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {},
+                handlers = {
+                    lsp_zero.default_setup,
+                },
+            })
 
             local cmp = require("cmp")
             local luasnip = require("luasnip")
 
             luasnip.config.setup({})
 
-            cmp.setup {
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
+            cmp.setup({
+                sources = {
+                    { name = "copilot" },
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
                 },
-                mapping = cmp.mapping.preset.insert {
+                mapping = cmp.mapping.preset.insert({
                     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete {},
@@ -89,31 +63,51 @@ return {
                             fallback()
                         end
                     end, { "i", "s" }),
-                },
-                sources = {
-                    { name = "copilot",  group_index = 2 },
-                    { name = "nvim_lsp", group_index = 2 },
-                    { name = "luasnip",  group_index = 2 },
-                },
-            }
-
-            local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                -- numhl = numhl to color linenumbers ---------------------vvv--
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = nil })
-            end
-
-            vim.diagnostic.config({
-                virtual_text = true,
-                underline = true,
-                -- virtual_text = {
-                --     source = "always",
-                -- },
-                float = {
-                    source = "always",
-                },
+                })
             })
         end,
-    },
+    }
 }
+
+
+
+-- Here"s the full list:
+
+-- * `K`: Displays hover information about the symbol under the cursor in a floating window. See [:help vim.lsp.buf.hover()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.hover()).
+
+-- * `gd`: Jumps to the definition of the symbol under the cursor. See [:help vim.lsp.buf.definition()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.definition()).
+
+-- * `gD`: Jumps to the declaration of the symbol under the cursor. Some servers don"t implement this feature. See [:help vim.lsp.buf.declaration()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.declaration()).
+
+-- * `gi`: Lists all the implementations for the symbol under the cursor in the quickfix window. See [:help vim.lsp.buf.implementation()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.implementation()).
+
+-- * `go`: Jumps to the definition of the type of the symbol under the cursor. See [:help vim.lsp.buf.type_definition()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.type_definition()).
+
+-- * `gr`: Lists all the references to the symbol under the cursor in the quickfix window. See [:help vim.lsp.buf.references()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.references()).
+
+-- * `gs`: Displays signature information about the symbol under the cursor in a floating window. See [:help vim.lsp.buf.signature_help()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.signature_help()). If a mapping already exists for this key this function is not bound.
+
+-- * `<F2>`: Renames all references to the symbol under the cursor. See [:help vim.lsp.buf.rename()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.rename()).
+
+-- * `<F3>`: Format code in current buffer. See [:help vim.lsp.buf.format()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.format()).
+
+-- * `<F4>`: Selects a code action available at the current cursor position. See [:help vim.lsp.buf.code_action()](https://neovim.io/doc/user/lsp.html#vim.lsp.buf.code_action()).
+
+-- * `gl`: Show diagnostics in a floating window. See [:help vim.diagnostic.open_float()](https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.open_float()).
+
+-- * `[d`: Move to the previous diagnostic in the current buffer. See [:help vim.diagnostic.goto_prev()](https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.goto_prev()).
+
+-- * `]d`: Move to the next diagnostic. See [:help vim.diagnostic.goto_next()](https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.goto_next()).
+
+-- By default lsp-zero will not create a keybinding if its "taken". This means if you already use one of these in your config, or some other plugins uses it ([which-key](https://github.com/folke/which-key.nvim) might be one), then lsp-zero"s bindings will not work.
+
+-- You can force lsp-zero"s bindings by adding `preserve_mappings = false` to [.default_keymaps()](https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/api-reference.md#default_keymapsopts).
+
+-- ```lua
+-- lsp_zero.on_attach(function(client, bufnr)
+--   lsp_zero.default_keymaps({
+--     buffer = bufnr,
+--     preserve_mappings = false
+--   })
+-- end)
+-- ```
