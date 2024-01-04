9e54ea2ebe519ed63d66ac4cefe8ba25fd8369a4
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-04 09:16:47 +0100

    Lazy Load Color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -25,6 +25,7 @@ return {
     -- },
     {
         "rebelot/kanagawa.nvim",
+        event = "VeryLazy",
         config = function()
             require("kanagawa").setup({
                 transparent = true,
