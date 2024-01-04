d6363e9869386af6fb324c5f45c936fa5545f3a4
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-04 07:43:50 +0100

    Trouble

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -75,9 +75,48 @@ return {
         },
     },
     {
-        "folke/trouble.nvim",
         "lewis6991/impatient.nvim",
-        enabled = true,
+        event = "VeryLazy",
+    },
+    {
+        "folke/trouble.nvim",
+        lazy = true,
+        cmd = { "TroubleToggle", "Trouble" },
+        opts = { icons = false, use_diagnostic_signs = true },
+        keys = {
+            { "<leader>ds", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
+            { "<leader>dw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+            { "<leader>dl", "<cmd>TroubleToggle loclist<cr>",               desc = "Location List (Trouble)" },
+            { "<leader>dq", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List (Trouble)" },
+            {
+                "<leader>dn",
+                function()
+                    if require("trouble").is_open() then
+                        require("trouble").previous({ skip_groups = true, jump = true })
+                    else
+                        local ok, err = pcall(vim.cmd.cprev)
+                        if not ok then
+                            vim.notify(err, vim.log.levels.ERROR)
+                        end
+                    end
+                end,
+                desc = "Previous trouble/quickfix item",
+            },
+            {
+                "<leader>dp",
+                function()
+                    if require("trouble").is_open() then
+                        require("trouble").next({ skip_groups = true, jump = true })
+                    else
+                        local ok, err = pcall(vim.cmd.cnext)
+                        if not ok then
+                            vim.notify(err, vim.log.levels.ERROR)
+                        end
+                    end
+                end,
+                desc = "Next trouble/quickfix item",
+            },
+        },
     },
     {
         "kdheepak/lazygit.nvim",
