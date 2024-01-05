971c057bbcdce6c7b4a145bf5eb37ea03b26af89
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-05 22:20:55 +0100

    Copilot

--- lua/settings.lua
+++ lua/settings.lua
@@ -44,3 +44,7 @@ vim.opt.shell = "/bin/zsh"
 vim.opt.relativenumber = true
 vim.opt.modifiable = true
 vim.opt.autochdir = false
+
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
