c306653b977efb12f54bf4b629fbb6ae9b0d4de6
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-06 07:25:24 +0100

    Better Tab

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,41 +38,21 @@ return {
                     { name = "buffer" },
                 },
                 mapping = cmp.mapping.preset.insert({
-                    -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    -- ["<C-Space>"] = cmp.mapping.complete {},
                     ["<CR>"] = cmp.mapping.confirm {
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
                     },
-                    -- ["<Tab>"] = cmp.mapping(function(fallback)
-                    --     local copilot_keys = vim.fn['copilot#Accept']()
-                    --     if cmp.visible() then
-                    --         cmp.select_next_item()
-                    --     elseif luasnip.expand_or_jumpable() then
-                    --         luasnip.expand_or_jump()
-                    --     elseif copilot_keys ~= '' and type(copilot_keys) == 'string' then
-                    --         vim.api.nvim_feedkeys(copilot_keys, 'i', true)
-                    --     else
-                    --         fallback()
-                    --     end
-                    -- end, { "i", "s" }),
-                    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    --     if cmp.visible() then
-                    --         cmp.select_prev_item()
-                    --     elseif luasnip.jumpable(-1) then
-                    --         luasnip.jump(-1)
-                    --     else
-                    --         fallback()
-                    --     end
-                    -- end, { "i", "s" }),
                     ['<C-p>'] = cmp.mapping.select_prev_item(),
                     ['<C-n>'] = cmp.mapping.select_next_item(),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                     ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<Tab>"] = cmp.mapping(function()
+                    ["<Tab>"] = cmp.mapping(function(fallback)
                         local copilot_keys = vim.fn['copilot#Accept']()
-                        vim.api.nvim_feedkeys(copilot_keys, 'i', true)
+                        if copilot_keys ~= '' and type(copilot_keys) == 'string' then
+                            vim.api.nvim_feedkeys(copilot_keys, 'i', true)
+                        else
+                            fallback()
+                        end
                     end, { "i", "s" }),
                 })
             })
