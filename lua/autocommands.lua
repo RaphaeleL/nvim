5a7ca9bac50e37f090f6ef758a80098e2c201978
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-06 22:11:52 +0100

    Lsp Keymaps with Autocmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -10,10 +10,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
--- Open NvimTree in Nvim Folder View (e.g. nvim .)
-vim.cmd [[
-  augroup ProjectDrawer
-    autocmd!
-    autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'cd '.argv()[0] | exe 'NvimTreeOpen' | wincmd p | ene | exe 'cd '.getcwd() | endif
-  augroup END
-]]
+-- Keymaps for LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LspAttach", {}),
+    callback = function()
+        local opts = { remap = false }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<F3>", ":lua vim.lsp.buf.format()<cr>", { desc = "LSP: Format" })
+        vim.keymap.set("n", "<F4>", function() vim.lsp.buf.code_action() end, opts)
+    end
+})
