b644b9d9f78f9d996f5e0e321bc3573270614f81
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-01-19 18:41:44 +0100

    Bash

--- lua/settings.lua
+++ lua/settings.lua
@@ -38,7 +38,7 @@ vim.opt.fillchars.eob = " "
 vim.opt.shortmess:append("c")
 vim.opt.whichwrap:append("<,>,[,],h,l")
 vim.opt.iskeyword:append("-")
-vim.opt.shell = "/bin/zsh"
+vim.opt.shell = "/bin/bash"
 vim.opt.relativenumber = true
 vim.opt.modifiable = true
 vim.opt.autochdir = false
