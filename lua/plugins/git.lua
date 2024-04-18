28e57c9f6db1e1c96c39a0f8a0b1afed0273e5be
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-04-18 08:52:05 +0200

    Forgot Diff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,6 +14,8 @@ return {
             { "<Leader>gc", ":Git commit<cr>", desc = " [G]it commit" },
             { "<Leader>gp", ":Git push<cr>", desc = " [G]it push" },
             { "<Leader>gP", ":Git pull --rebase<cr>", desc = " [G]it pull --rebase" },
+            { "<Leader>gd", ":Git diff<cr>", desc = " [G]it diff" },
         }
     },
+
 }
