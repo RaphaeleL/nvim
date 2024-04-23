8f83280c2e8199986f1792d5db2ff5701632c113
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-04-23 20:39:53 +0200

    Gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,4 +17,32 @@ return {
             { "<Leader>gd", ":Git diff<cr>", desc = " [G]it diff" },
         }
     },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = function()
+            local ok, gitsigns = pcall(require, "gitsigns")
+            if not ok then
+                require("notify")("Couldn't require gitsigns", "error")
+                return
+            end
+            gitsigns.setup {
+                -- numhl = true,
+                signs = {
+                    add = { text = "▎" },
+                    change = { text = "▎" },
+                    delete = { text = "󰐊" },
+                    topdelete = { text = "󰐊" },
+                    changedelete = { text = "▎" },
+                    untracked = { text = "▎" },
+                },
+            }
+        end,
+        keys = {
+            { "<Leader>gd", ":Gitsigns toggle_deleted<cr>", desc = " Gitsigns: [G]it [t]oggle deleted" },
+            { "<Leader>gn", ":Gitsigns toggle_numhl<cr>",   desc = " Gitsigns: [G]it [t]oggle Line Number Signs" },
+            { "<Leader>gs", ":Gitsigns toggle_signs<cr>",   desc = " Gitsigns: [G]it [t]oggle Signs" },
+        }
+    },
+
 }
