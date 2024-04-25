3074f0b47d46ee1c0e64d29a4ba9915254718258
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-04-25 08:10:57 +0200

    TJ's Express Line
    a bit ugly ig, so just commented out.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,5 +26,58 @@ return {
         config = function(_, opts)
             require("illuminate").configure(opts)
         end,
-    }
+    },
+    -- {
+    --     "tjdevries/express_line.nvim",
+    --     dependencies = {
+    --         "nvim-tree/nvim-web-devicons"
+    --     },
+    --     config = function()
+    --         vim.opt.laststatus = 2
+    --         vim.opt.cmdheight = 1
+    --         vim.opt.showcmd = true
+    --         require("el").setup({
+    --             generator = function(win_id)
+    --                 local segments = {}
+    --                 local builtin = require("el.builtin")
+    --                 local extensions = require("el.extensions")
+    --                 local subscribe = require("el.subscribe")
+    --
+    --                 -- Mode
+    --                 table.insert(segments, extensions.mode)
+    --
+    --                 -- Git Branch
+    --                 table.insert(segments,
+    --                     subscribe.buf_autocmd(
+    --                         "el_git_branch",
+    --                         "BufEnter",
+    --                         function(window, buffer)
+    --                             local branch = extensions.git_branch(window, buffer)
+    --                             if branch then
+    --                                 return branch
+    --                             end
+    --                         end
+    --                     ))
+    --
+    --                 -- Git Changes 
+    --                 table.insert(segments,
+    --                     subscribe.buf_autocmd(
+    --                         "el_git_status",
+    --                         "BufWritePost",
+    --                         function(window, buffer)
+    --                             local changes = extensions.git_changes(window, buffer)
+    --                             if changes then
+    --                                 return changes
+    --                             end
+    --                         end
+    --                     ))
+    --
+    --                 -- File
+    --                 table.insert(segments, builtin.file)
+    --
+    --                 return segments
+    --             end
+    --         })
+    --     end
+    -- }
 }
