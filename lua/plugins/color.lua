26b669217eeae6348351aea84c283d8ad5d8adc2
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-05-15 08:23:31 +0200

    Improve Kanagawa

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,4 +1,45 @@
 return {
+    {
+        "rebelot/kanagawa.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("kanagawa").setup({
+                transparent = true,
+                overrides = function(colors)
+                    return {
+                        StatusLine = { bg = "NONE" },
+                        StatusLineNC = { bg = "NONE" },
+                        TabLineSel = { bg = "NONE" },
+                        LineNr = { bg = "NONE" },
+                        CursorLineNr = { bg = "NONE" },
+                        SignColumn = { bg = "NONE" },
+                        GitSignsAdd = { bg = "NONE" },
+                        GitSignsChange = { bg = "NONE" },
+                        GitSignsDelete = { bg = "NONE" },
+                        DiagnosticSignError = { bg = "NONE" },
+                        DiagnosticSignWarn = { bg = "NONE" },
+                        DiagnosticSignInfo = { bg = "NONE" },
+                        DiagnosticSignHint = { bg = "NONE" },
+                        TelescopeBorder = { bg = "NONE" },
+                        TelescopeTitle = { fg = colors.theme.ui.special, bold = true },
+                        TelescopePromptNormal = { bg = colors.theme.ui.bg_p1 },
+                        TelescopePromptBorder = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
+                        TelescopeResultsNormal = { fg = colors.theme.ui.fg_dim, bg = colors.theme.ui.bg_m1 },
+                        TelescopeResultsBorder = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
+                        TelescopePreviewNormal = { bg = colors.theme.ui.bg_dim },
+                        TelescopePreviewBorder = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
+                        Todo = { bg = colors.theme.ui.bg_p2 },
+                        -- VisualNonText = { fg = colors.theme.ui.bg_p3, bg = "#223249" },
+                    }
+                end,
+                background = {
+                    dark = "dragon",
+                    light = "lotus"
+                }
+            })
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
     -- {
     --     "scottmckendry/cyberdream.nvim",
     --     event = "VeryLazy",
@@ -34,7 +75,7 @@ return {
     --                 CursorLineNr = { fg = "gold" },
     --             },
     --         })
-    --         -- vim.cmd("colorscheme rose-pine")
+    --         vim.cmd("colorscheme rose-pine")
     --     end
     -- },
     -- {
@@ -55,46 +96,6 @@ return {
     --         vim.cmd("colorscheme catppuccin")
     --     end
     -- },
-    {
-        "rebelot/kanagawa.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("kanagawa").setup({
-                transparent = true,
-                overrides = function(colors)
-                    return {
-                        StatusLine = { bg = "NONE" },
-                        StatusLineNC = { bg = "NONE" },
-                        TabLineSel = { bg = "NONE" },
-                        LineNr = { bg = "NONE" },
-                        CursorLineNr = { bg = "NONE" },
-                        SignColumn = { bg = "NONE" },
-                        GitSignsAdd = { bg = "NONE" },
-                        GitSignsChange = { bg = "NONE" },
-                        GitSignsDelete = { bg = "NONE" },
-                        DiagnosticSignError = { bg = "NONE" },
-                        DiagnosticSignWarn = { bg = "NONE" },
-                        DiagnosticSignInfo = { bg = "NONE" },
-                        DiagnosticSignHint = { bg = "NONE" },
-                        TelescopeBorder = { bg = "NONE" },
-                        TelescopeTitle = { fg = colors.theme.ui.special, bold = true },
-                        TelescopePromptNormal = { bg = colors.theme.ui.bg_p1 },
-                        TelescopePromptBorder = { fg = colors.theme.ui.bg_p1, bg = colors.theme.ui.bg_p1 },
-                        TelescopeResultsNormal = { fg = colors.theme.ui.fg_dim, bg = colors.theme.ui.bg_m1 },
-                        TelescopeResultsBorder = { fg = colors.theme.ui.bg_m1, bg = colors.theme.ui.bg_m1 },
-                        TelescopePreviewNormal = { bg = colors.theme.ui.bg_dim },
-                        TelescopePreviewBorder = { bg = colors.theme.ui.bg_dim, fg = colors.theme.ui.bg_dim },
-                        Todo = { bg = colors.theme.ui.bg_p2 },
-                    }
-                end,
-                background = {
-                    dark = "dragon",
-                    light = "lotus"
-                }
-            })
-            vim.cmd("colorscheme kanagawa")
-        end
-    },
     -- {
     --     "ellisonleao/gruvbox.nvim",
     --     event = "VeryLazy",
