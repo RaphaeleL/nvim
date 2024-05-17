53dc3c62a3bfdf23e9c8f9f84325a0e85e27e88a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-05-17 12:28:51 +0200

    Just Testing

--- lua/plugins/version_controll.lua
+++ lua/plugins/version_controll.lua
@@ -10,11 +10,11 @@ return {
         "tpope/vim-fugitive",
         lazy = true,
         keys = {
-            { "<Leader>ga", ":Git add .<cr>", desc = " [G]it add ." },
-            { "<Leader>gc", ":Git commit<cr>", desc = " [G]it commit" },
-            { "<Leader>gp", ":Git push<cr>", desc = " [G]it push" },
-            { "<Leader>gP", ":Git pull --rebase<cr>", desc = " [G]it pull --rebase" },
-            { "<Leader>gd", ":Git diff<cr>", desc = " [G]it diff" },
+            { "<Leader>ga", ":Git add .<cr><cr>", desc = " [G]it add ." },
+            { "<Leader>gc", ":Git commit<cr><cr>", desc = " [G]it commit" },
+            { "<Leader>gp", ":Git push<cr><cr>", desc = " [G]it push" },
+            { "<Leader>gP", ":Git pull --rebase<cr><cr>", desc = " [G]it pull --rebase" },
+            { "<Leader>gd", ":Git diff<cr><cr>", desc = " [G]it diff" },
         }
     },
     {
