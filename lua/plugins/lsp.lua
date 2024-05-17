3787d4f62991fad334c5cf16269022ca7441146d
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-05-17 10:00:07 +0200

    Icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -122,7 +122,13 @@ return {
                 }, {
                     { name = 'buffer' },
                 })
-            },
+            })
+
+            local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
 
             vim.diagnostic.config({
                 virtual_text = true,
@@ -135,7 +141,7 @@ return {
                     header = "",
                     prefix = "",
                 },
-            }))
+            })
 
         end
     },
