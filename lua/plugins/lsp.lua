6f482d28177e3107852da1f6c885bc73d3f8eb5a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-06-28 07:15:35 +0200

    Better Iconskj

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,7 +55,7 @@ return {
                 },
             })
 
-            local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
+            local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticSign" .. type
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
