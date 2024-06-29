5a4a12adbe1719569bbed3fa2e873bf0b44d21c2
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-06-29 09:09:02 +0200

    Declutter
    - I dont use snippets
    - I dont wanna use ai cauz of skill issues (i dont use my brain oO)

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -39,12 +39,7 @@ return {
                     select = true,
                 },
                 ["<Tab>"] = cmp.mapping(function(fallback)
-                    local copilot_keys = vim.fn["copilot#Accept"]()
-                    if copilot_keys ~= '' and type(copilot_keys) == "string" then
-                        vim.api.nvim_feedkeys(copilot_keys, "i", true)
-                    else
-                        fallback()
-                    end
+                    fallback()
                 end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
