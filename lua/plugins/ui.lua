224df9924590c8a1a5c95b9aec8547adb9a009dc
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-07-02 14:50:49 +0200

    fix treesitter

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,17 +11,50 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-		build = ":TSUpdate",
-		branch = "main",
 		config = function()
+			-- require("nvim-treesitter").setup({
+			-- 	ensure_install = { "core", "stable" },
+			-- })
+
+			local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
+
 			require("nvim-treesitter").setup({
-				ensure_installed = "maintained",
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = true,
-				},
+				ensure_install = { "core", "stable" },
+			})
+
+			local syntax_on = {
+				elixir = true,
+				php = true,
+			}
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				callback = function(args)
+					local bufnr = args.buf
+					local ft = vim.bo[bufnr].filetype
+					pcall(vim.treesitter.start)
+
+					if syntax_on[ft] then
+						vim.bo[bufnr].syntax = "on"
+					end
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "TSUpdate",
+				callback = function()
+					local parsers = require("nvim-treesitter.parsers")
+
+					parsers.lua = {
+						tier = 0,
+
+						---@diagnostic disable-next-line: missing-fields
+						install_info = {
+							path = "~/plugins/tree-sitter-lua",
+							files = { "src/parser.c", "src/scanner.c" },
+						},
+					}
+				end,
 			})
 		end,
 	},
