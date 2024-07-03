a70e49fd177c4c4147f63570f7a7d4fd12bc72a6
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-07-03 13:03:11 +0200

    Lazy Load Completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,11 +1,12 @@
 return {
 	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"onsails/lspkind.nvim",
+		{ "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
+		{ "hrsh7th/cmp-buffer", event = "InsertEnter" },
+		{ "hrsh7th/cmp-path", event = "InsertEnter" },
+		{ "hrsh7th/cmp-cmdline", event = "InsertEnter" },
+		{ "onsails/lspkind.nvim", event = "InsertEnter" },
 	},
 	config = function()
 		require("plugins.settings.completion")
