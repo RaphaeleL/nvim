21611ea16476d9f43a57eef37a8ed67dabbc9cdf
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-07-26 11:17:23 +0200

    Refactor

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -172,6 +172,13 @@ return {
 			vim.keymap.set("n", "<leader>sp", function()
 				toggle_telescope(harpoon:list())
 			end, { desc = "Telescope: [S]how Harpoon Window" })
+
+			-- Set <space>1..<space>5 be my shortcuts to moving to the files
+			for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+				vim.keymap.set("n", string.format("<space>%d", idx), function()
+					harpoon:list():select(idx)
+				end)
+			end
 		end,
 	},
 	{
