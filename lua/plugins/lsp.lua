7c41b25750b8da76933753a056df9035c5dcd220
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-30 15:55:43 +0200

    Ooops

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -145,7 +145,7 @@ return {
 				vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
 
-				vim.keymap.set("n", "<space>R", vim.lsp.buf.rename, { buffer = 0 })
+				vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
 				vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
 
 				local filetype = vim.bo[bufnr].filetype
