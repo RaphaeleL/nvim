fdd966d1afc0df829a1b2d8c45623566f06847ac
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-30 16:53:50 +0200

    Coloring

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -87,16 +87,18 @@ return {
 				red = "#ff5189",
 				violet = "#d183e8",
 				grey = "#303030",
+				yellow = "#f0c674",
+				orange = "#de935f",
 			}
 
 			local bubbles_theme = {
 				normal = {
-					a = { fg = colors.black, bg = colors.blue },
+					a = { fg = colors.black, bg = colors.orange },
 					b = { fg = colors.white, bg = colors.grey },
 					c = { fg = colors.white },
 				},
 
-				insert = { a = { fg = colors.black, bg = colors.cyan } },
+				insert = { a = { fg = colors.black, bg = colors.blue } },
 				visual = { a = { fg = colors.black, bg = colors.cyan } },
 				replace = { a = { fg = colors.black, bg = colors.red } },
 
