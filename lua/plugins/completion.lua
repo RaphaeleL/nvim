239faaec631d3fa70c516b6a49e078f830499f3e
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-08-30 15:52:45 +0200

    Better Copilot?!

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,6 +10,21 @@ return {
 	},
 	config = function()
 		local cmp = require("cmp")
+		local lspkind = require("lspkind")
+		lspkind.init({
+			symbol_map = {
+				Copilot = "",
+			},
+		})
+
+		local has_words_before = function()
+			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+				return false
+			end
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+		end
+
 		local cmp_settings = {
 			formatting = {
 				fields = { "kind", "abbr", "menu" },
@@ -31,10 +46,16 @@ return {
 					select = true,
 				}),
 				["<Tab>"] = cmp.mapping(function(fallback)
-					fallback()
+					if cmp.visible() and has_words_before() then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					else
+						fallback()
+					end
+					-- fallback()
 				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
+				{ name = "copilot" },
 				{ name = "nvim_lsp" },
 			}, {
 				{ name = "buffer" },
