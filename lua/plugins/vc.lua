ffe12fcb52994695e20435bff44bbb31e10cd809
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-09-02 09:18:23 +0200

    No Lazygit

--- lua/plugins/vc.lua
+++ lua/plugins/vc.lua
@@ -1,12 +1,5 @@
 return {
 	{
-		"kdheepak/lazygit.nvim",
-		lazy = true,
-		keys = {
-			{ "<Leader>gg", ":LazyGit<cr>", desc = " [G]it: Lazy[G]it" },
-		},
-	},
-	{
 		"tpope/vim-fugitive",
 		lazy = true,
 		keys = {
