2898786680675351246b8211f74e7d6d15b4fb03
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2024-10-17 08:16:52 +0200

    Declutter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,42 +6,5 @@ return {
 	{
 		"lewis6991/impatient.nvim",
 		event = "VeryLazy",
-	},
-	-- {
-	-- 	"Zeioth/hot-reload.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"rcarriga/nvim-notify",
-	-- 	},
-	-- 	event = "BufEnter",
-	-- 	opts = function()
-	-- 		local config_dir = vim.fn.stdpath("config")
-	-- 		local files = {
-	-- 			config_dir .. "/init.lua",
-	-- 			config_dir .. "/lua/plugins/ai.lua",
-	-- 			config_dir .. "/lua/plugins/color.lua",
-	-- 			config_dir .. "/lua/plugins/completion.lua",
-	-- 			config_dir .. "/lua/plugins/init.lua",
-	-- 			config_dir .. "/lua/plugins/lsp.lua",
-	-- 			config_dir .. "/lua/plugins/ui.lua",
-	-- 			config_dir .. "/lua/plugins/utils.lua",
-	-- 			config_dir .. "/lua/plugins/vc.lua",
-	-- 			config_dir .. "/plugin/autocommands.lua",
-	-- 			config_dir .. "/plugin/color.lua",
-	-- 			config_dir .. "/plugin/keymaps.lua",
-	-- 			config_dir .. "/plugin/options.lua",
-	-- 		}
-	-- 		return {
-	-- 			reload_files = files,
-	-- 			notify = false,
-	-- 			reload_callback = function()
-	-- 				local plugin = "Hot Reloading"
-	-- 				vim.notify = require("notify")
-	-- 				vim.notify("Reloading Files ... \n - " .. vim.fn.expand("%:p"), "info", {
-	-- 					title = plugin,
-	-- 				})
-	-- 			end,
-	-- 		}
-	-- 	end,
-	-- },
+	}
 }
