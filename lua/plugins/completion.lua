83612312f427d4c89ed30b36fa584d5515e0335f
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-10 10:40:08 +0100

    Format

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,12 +2,11 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-        { "hrsh7th/cmp-nvim-lsp",   event = "InsertEnter" },
-        { "hrsh7th/cmp-buffer",     event = "InsertEnter" },
-        { "hrsh7th/cmp-path",       event = "InsertEnter" },
-        { "hrsh7th/cmp-cmdline",    event = "InsertEnter" },
-        { "onsails/lspkind.nvim",   event = "InsertEnter" },
-        { "zbirenbaum/copilot-cmp", event = "InsertEnter" },
+        { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
+        { "hrsh7th/cmp-buffer",   event = "InsertEnter" },
+        { "hrsh7th/cmp-path",     event = "InsertEnter" },
+        { "hrsh7th/cmp-cmdline",  event = "InsertEnter" },
+        { "onsails/lspkind.nvim", event = "InsertEnter" },
     },
     config = function()
         local cmp = require("cmp")
