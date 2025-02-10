5232b2d4dd8ef85bef0cfdcdc202ac129605443a
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-10 10:40:04 +0100

    Update AI

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,6 +1,23 @@
 return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {},
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+        suggestion = {
+            enabled = not vim.g.ai_cmp,
+            auto_trigger = true,
+            hide_during_completion = vim.g.ai_cmp,
+            keymap = {
+                accept = "<M-l>",
+                next = "<M-n>",
+                prev = "<M-p>",
+            },
+        },
+        panel = { enabled = false },
+        filetypes = {
+            markdown = true,
+            help = true,
+        },
+    },
 }
