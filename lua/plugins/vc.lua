3c25a978aee5aa68df0eb2168edab689af399de0
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-10 10:46:45 +0100

    Format

--- lua/plugins/vc.lua
+++ lua/plugins/vc.lua
@@ -3,11 +3,11 @@ return {
 		"tpope/vim-fugitive",
 		lazy = true,
 		keys = {
-			{ "<Leader>ga", ":Git add .<cr>", desc = " [G]it add ." },
-			{ "<Leader>gc", ":Git commit<cr>", desc = " [G]it commit" },
-			{ "<Leader>gp", ":Git push<cr>", desc = " [G]it push" },
+			{ "<Leader>ga", ":Git add .<cr>",         desc = " [G]it add ." },
+			{ "<Leader>gc", ":Git commit<cr>",        desc = " [G]it commit" },
+			{ "<Leader>gp", ":Git push<cr>",          desc = " [G]it push" },
 			{ "<Leader>gP", ":Git pull --rebase<cr>", desc = " [G]it pull --rebase" },
-			{ "<Leader>gd", ":Git diff<cr>", desc = " [G]it diff" },
+			{ "<Leader>gd", ":Git diff<cr>",          desc = " [G]it diff" },
 		},
 	},
 	{
@@ -26,8 +26,8 @@ return {
 		},
 		keys = {
 			{ "<Leader>gr", ":Gitsigns toggle_deleted<cr>", desc = " Gitsigns: [G]it [t]oggle deleted" },
-			{ "<Leader>gn", ":Gitsigns toggle_numhl<cr>", desc = " Gitsigns: [G]it [t]oggle Line Number Signs" },
-			{ "<Leader>gs", ":Gitsigns toggle_signs<cr>", desc = " Gitsigns: [G]it [t]oggle Signs" },
+			{ "<Leader>gn", ":Gitsigns toggle_numhl<cr>",   desc = " Gitsigns: [G]it [t]oggle Line Number Signs" },
+			{ "<Leader>gs", ":Gitsigns toggle_signs<cr>",   desc = " Gitsigns: [G]it [t]oggle Signs" },
 		},
 	},
 }
