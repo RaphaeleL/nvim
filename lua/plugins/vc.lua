7433ac68b5a6c030d9450855e3ae79c475b71f33
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-12 07:41:21 +0100

    cleanup, lazygit

--- lua/plugins/vc.lua
+++ lua/plugins/vc.lua
@@ -25,29 +25,16 @@ return {
             },
         },
         keys = {
-            { "<Leader>gr", ":Gitsigns toggle_deleted<cr>", desc = " Gitsigns: [G]it [t]oggle deleted" },
+            { "<Leader>gR", ":Gitsigns toggle_deleted<cr>", desc = " Gitsigns: [G]it [t]oggle deleted" },
             { "<Leader>gn", ":Gitsigns toggle_numhl<cr>",   desc = " Gitsigns: [G]it [t]oggle Line Number Signs" },
             { "<Leader>gs", ":Gitsigns toggle_signs<cr>",   desc = " Gitsigns: [G]it [t]oggle Signs" },
         },
     },
     {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        keys = {
+            { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Lazy [G]it" }
+        }
     }
-}
 }
