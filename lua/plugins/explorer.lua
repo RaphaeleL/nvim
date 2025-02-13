6b13e97569f5c0dd3bcd271f85a8452e19bdb503
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-02-13 09:46:53 +0100

    No icons

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -57,57 +57,15 @@ return {
             },
             filters = { custom = { "^.git$" } },
             renderer = {
-                -- root_folder_modifier = ":t",
                 full_name = true,
                 group_empty = true,
                 indent_markers = {
                     enable = true,
                 },
-                icons = {
-                    show = {
-                        file = false,
-                        folder = false,
-                        folder_arrow = false,
-                        git = false,
-                        modified = false,
-                        hidden = false,
-                        diagnostics = false,
-                        bookmarks = false,
-                    },
-                    glyphs = {
-                        default = "",
-                        symlink = "",
-                        folder = {
-                            arrow_open = "",
-                            arrow_closed = "",
-                            default = "",
-                            open = "",
-                            empty = "",
-                            empty_open = "",
-                            symlink = "",
-                            symlink_open = "",
-                        },
-                        git = {
-                            unstaged = "",
-                            staged = "S",
-                            unmerged = "",
-                            renamed = "➜",
-                            untracked = "U",
-                            deleted = "",
-                            ignored = "◌",
-                        },
-                    },
-                },
             },
             diagnostics = {
                 enable = false,
                 show_on_dirs = true,
-                icons = {
-                    hint = "",
-                    info = "",
-                    warning = "",
-                    error = "",
-                },
             },
             view = {
                 width = 40,
@@ -150,7 +108,6 @@ return {
             experimental_watch_for_changes = false,
             use_default_keymaps = true,
             columns = {
-                -- "icon",
                 "permissions",
                 "size",
                 "mtime",
