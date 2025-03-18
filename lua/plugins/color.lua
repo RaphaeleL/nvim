0b939e3ebf5fe885678dba6e37eeb77606551c42
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-03-18 12:34:48 +0100

    Moon in Main

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             require("rose-pine").setup({
                 variant = "auto",
-                dark_variant = "main",
+                dark_variant = "moon",
                 dim_inactive_windows = true,
                 extend_background_behind_borders = true,
                 dim_nc_background = true,
