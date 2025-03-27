b51ac3e28625f240c5c0e7c4da6e16ac2045d28b
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-03-27 13:15:24 +0100

    Declutter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,13 +36,6 @@ return {
 		-- Default LSP Settings for all LSP servers
 		vim.lsp.config("*", {
 			capabilities = require('blink.cmp').get_lsp_capabilities(),
-			-- capabilities = {
-			-- 	textDocument = {
-			-- 		semanticTokens = {
-			-- 			multilineTokenSupport = true,
-			-- 		},
-			-- 	},
-			-- },
 			root_markers = { ".git" },
 		})
 
@@ -51,26 +44,10 @@ return {
 
 		-- Attach LSP servers
 		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("my.lsp", {}), -- NOTE: is my.lsp a dummy name ?
 			callback = function(args)
-				local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-				if client:supports_method("textDocument/implementation") then
-					-- Create a keymap for vim.lsp.buf.implementation ...
-				end
-
-				-- if client:supports_method("textDocument/completion") then
-				-- 	-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-				-- 	-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-				-- 	-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-				-- 	-- client.server_capabilities.completionProvider.triggerCharacters = chars
-				--
-					vim.keymap.set("n", "gf", ":lua require('conform').format()<cr>", { desc = "LSP: Format" })
-					vim.keymap.set("n", "gR", ":lua vim.lsp.buf.rename()<cr>", { silent = true })
-					vim.keymap.set("n", "K", ":lua vim.lsp.buf.signature_help()<cr>", { silent = true })
-				--
-				-- 	vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-				-- end
+                vim.keymap.set("n", "gf", ":lua require('conform').format()<cr>", { desc = "LSP: Format" })
+                vim.keymap.set("n", "gR", ":lua vim.lsp.buf.rename()<cr>", { silent = true })
+                vim.keymap.set("n", "K", ":lua vim.lsp.buf.signature_help()<cr>", { silent = true })
 			end,
 		})
 
