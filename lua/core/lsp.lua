74b8985c79c3ca047cf7e1522250e2030499ac7d
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-03-27 09:26:59 +0100

    Commenting

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,3 +1,4 @@
+-- Default LSP Settings for all LSP servers
 vim.lsp.config('*', {
     capabilities = {
         textDocument = {
@@ -8,25 +9,36 @@ vim.lsp.config('*', {
     },
     root_markers = { '.git' },
 })
+
+-- Diagnostic settings
 vim.diagnostic.config({ virtual_lines = false, virtual_text = false })
+
+-- Attach LSP servers
 vim.api.nvim_create_autocmd('LspAttach', {
-    -- group = vim.api.nvim_create_augroup('my.lsp', {}),
+    group = vim.api.nvim_create_augroup('my.lsp', {}), -- NOTE: is my.lsp a dummy name ?
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
         if client:supports_method('textDocument/implementation') then
             -- Create a keymap for vim.lsp.buf.implementation ...
         end
 
-        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
         if client:supports_method('textDocument/completion') then
+            -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
             -- Optional: trigger autocompletion on EVERY keypress. May be slow!
             -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
             -- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+            vim.keymap.set('n', 'gR', ':lua vim.lsp.buf.rename()<cr>', {silent = true})
+            vim.keymap.set('n', 'K', ':lua vim.lsp.buf.signature_help()<cr>', {silent = true})
 
             vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
         end
 
     end,
 })
+
+-- Enable LSP for specific languages
+-- NOTE: those names are the files inside of `~/.config/nvim/lsp/*.lua`
 vim.lsp.enable({'python', 'c_cpp', 'lua', 'typescript'})
 
