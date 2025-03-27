5095c0aef20ae893089adbb69ae352fe9e7264aa
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-03-27 09:55:17 +0100

    Outsource

--- init.lua
+++ init.lua
@@ -1,3 +1,25 @@
-require("core.init")
-require("core.bootstrap")
+-- if vim.fn.has("nvim-0.11") == 0 then
+--     vim.notify("This Configuration only supports Neovim 0.11+", vim.log.levels.ERROR)
+--     return
+-- end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--single-branch",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
+end
+
+vim.opt.runtimepath:prepend(lazypath)
+require("lazy").setup({ import = "plugins" }, { change_detection = { notify = false } })
+
 require("core.lsp")
--- lua/core/init.lua
+++
@@ -1,8 +0,0 @@
--- if vim.fn.has("nvim-0.11") == 0 then
---     vim.notify("This Configuration only supports Neovim 0.11+", vim.log.levels.ERROR)
---     return
--- end
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
