37ccae03ef2678402772a8265d7165a0602757b7
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-03-27 09:26:54 +0100

    No more Vim Diagnostics in Lua

--- lsp/lua.lua
+++ lsp/lua.lua
@@ -7,6 +7,9 @@ return {
             telemetry = {
                 enable = false,
             },
+            diagnostics = {
+                globals = { 'vim' },
+            },
         },
     },
 }
