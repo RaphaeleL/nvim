96643db652817b50dcee21d152f9d7bd4e0459f0
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-03-31 07:19:16 +0200

    New LSP

---
+++ lsp/tailwind.lua
@@ -0,0 +1,9 @@
+return {
+    cmd = { "tailwindcss-language-server" },
+    root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+    init_options = {
+        hostInfo = "neovim",
+    },
+}
+
