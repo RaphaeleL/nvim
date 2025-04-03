a79f5e8fd67bcfbaef013acf7743c141442fb4ae
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-04-03 08:12:59 +0200

    Fix: Use Both Versions of LSP

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,11 +3,9 @@ local function has_words_before()
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
----@type function?, function?
 local icon_provider, hl_provider
 
 local function get_kind_icon(CTX)
-    -- Evaluate icon provider
     if not icon_provider then
         local _, mini_icons = pcall(require, "mini.icons")
         if _G.MiniIcons then
@@ -42,7 +40,6 @@ local function get_kind_icon(CTX)
         end
         if not icon_provider then icon_provider = function() end end
     end
-    -- Evaluate highlight provider
     if not hl_provider then
         local highlight_colors_avail, highlight_colors = pcall(require, "nvim-highlight-colors")
         if highlight_colors_avail then
@@ -63,84 +60,191 @@ local function get_kind_icon(CTX)
         end
         if not hl_provider then hl_provider = function() end end
     end
-    -- Call resolved providers
     icon_provider(CTX)
     hl_provider(CTX)
-    -- Return text and highlight information
     return { text = CTX.kind_icon .. CTX.icon_gap, highlight = CTX.kind_hl }
 end
 
+local servers = {
+    bashls = true,
+    lua_ls = {
+        server_capabilities = { semanticTokensProvider = vim.NIL, },
+    },
+    clangd = {
+        init_options = { clangdFileStatus = true },
+        filetypes = { "c", "cpp" },
+    },
+    pyright = {
+        settings = {
+            python = {
+                analysis = {
+                    autoSearchPaths = true,
+                    diagnosticMode = "workspace",
+                    useLibraryCodeForTypes = true,
+                },
+            },
+        },
+    },
+    tailwindcss = true,
+    ts_ls = { enabled = true, },
+}
+local disable_semantic_tokens = { lua = true }
+local ensure_installed = { "pyright", "lua_ls", "bashls", "clangd", "vimls", "ts_ls", "tailwindcss" }
+
+local SHOULD_USE_NEW_LSP = false
+
 return {
     "Saghen/blink.cmp",
-    event = { "InsertEnter", "CmdlineEnter" },
-    opts_extend = { "sources.default", "cmdline.sources", "term.sources" },
-    opts = {
-        sources = { default = { "lsp", "path", "snippets", "buffer" }, },
-        keymap = {
-            ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-            ["<Up>"] = { "select_prev", "fallback" },
-            ["<Down>"] = { "select_next", "fallback" },
-            ["<C-N>"] = { "select_next", "show" },
-            ["<C-P>"] = { "select_prev", "show" },
-            ["<C-J>"] = { "select_next", "fallback" },
-            ["<C-K>"] = { "select_prev", "fallback" },
-            ["<C-U>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-D>"] = { "scroll_documentation_down", "fallback" },
-            ["<C-e>"] = { "hide", "fallback" },
-            ["<CR>"] = { "accept", "fallback" },
-            ["<Tab>"] = {
-                "snippet_forward",
-                "select_next",
-                function(cmp)
-                    if has_words_before() or vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
-                end,
-                "fallback",
+    -- event = { "InsertEnter", "CmdlineEnter" },
+    -- opts_extend = { "sources.default", "cmdline.sources", "term.sources" },
+    dependencies = not SHOULD_USE_NEW_LSP and {
+	    "neovim/nvim-lspconfig",
+		"folke/neodev.nvim",
+        "williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+    config = function()
+        require("blink.cmp").setup({
+            sources = { default = { "lsp", "path", "snippets", "buffer" }, },
+            keymap = {
+                ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<Up>"] = { "select_prev", "fallback" },
+                ["<Down>"] = { "select_next", "fallback" },
+                ["<C-N>"] = { "select_next", "show" },
+                ["<C-P>"] = { "select_prev", "show" },
+                ["<C-J>"] = { "select_next", "fallback" },
+                ["<C-K>"] = { "select_prev", "fallback" },
+                ["<C-U>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-D>"] = { "scroll_documentation_down", "fallback" },
+                ["<C-e>"] = { "hide", "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
+                ["<Tab>"] = {
+                    "snippet_forward",
+                    "select_next",
+                    function(cmp)
+                        if has_words_before() or vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+                    end,
+                    "fallback",
+                },
+                ["<S-Tab>"] = {
+                    "snippet_backward",
+                    "select_prev",
+                    function(cmp)
+                        if vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+                    end,
+                    "fallback",
+                },
             },
-            ["<S-Tab>"] = {
-                "snippet_backward",
-                "select_prev",
-                function(cmp)
-                    if vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
-                end,
-                "fallback",
+            fuzzy = {
+                implementation = 'lua'
             },
-        },
-        fuzzy = {
-            implementation = 'lua'
-        },
-        completion = {
-            list = { selection = { preselect = false, auto_insert = true } },
-            menu = {
-                auto_show = function(ctx) return ctx.mode ~= "cmdline" end,
-                border = "rounded",
-                winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-                draw = {
-                    treesitter = { "lsp" },
-                    components = {
-                        kind_icon = {
-                            text = function(ctx) return get_kind_icon(ctx).text end,
-                            highlight = function(ctx) return get_kind_icon(ctx).highlight end,
+            completion = {
+                list = { selection = { preselect = false, auto_insert = true } },
+                menu = {
+                    auto_show = function(ctx) return ctx.mode ~= "cmdline" end,
+                    border = "rounded",
+                    winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+                    draw = {
+                        treesitter = { "lsp" },
+                        components = {
+                            kind_icon = {
+                                text = function(ctx) return get_kind_icon(ctx).text end,
+                                highlight = function(ctx) return get_kind_icon(ctx).highlight end,
+                            },
                         },
                     },
                 },
-            },
-            accept = {
-                auto_brackets = { enabled = true },
-            },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 0,
-                window = {
-                    border = "rounded",
-                    winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+                accept = {
+                    auto_brackets = { enabled = true },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 0,
+                    window = {
+                        border = "rounded",
+                        winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+                    },
                 },
             },
-        },
-        signature = {
-            window = {
-                border = "rounded",
-                winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
+            signature = {
+                window = {
+                    border = "rounded",
+                    winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
+                },
             },
-        },
-    }
+        })
+        if SHOULD_USE_NEW_LSP then 
+            vim.lsp.config("*", {
+                capabilities = require('blink.cmp').get_lsp_capabilities(),
+                root_markers = { ".git" },
+            })
+            vim.diagnostic.config({ virtual_lines = false, virtual_text = false })
+            vim.lsp.enable({ "c", "lua", "python", "tailwind", 'typescript' })
+        else
+            require("neodev").setup({})
+
+            local servers_to_install = vim.tbl_filter(function(key)
+                local t = servers[key]
+                if type(t) == "table" then
+                    return not t.manual_install
+                else
+                    return t
+                end
+            end, vim.tbl_keys(servers))
+
+            require("mason").setup({ PATH = "prepend" })
+
+            vim.list_extend(ensure_installed, servers_to_install)
+            require("mason-tool-installer").setup({
+                ensure_installed = ensure_installed,
+                automatic_installation = true,
+            })
+
+            for name, config in pairs(servers) do
+                if config == true then config = {} end
+                config = vim.tbl_deep_extend("force", {}, {
+                    ocapabilities = require('blink.cmp').get_lsp_capabilities(),f
+                }, config)
+
+                require("lspconfig")[name].setup(config)
+            end
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local bufnr = args.buf
+                    local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+                    local settings = servers[client.name]
+                    if type(settings) ~= "table" then
+                        settings = {}
+                    end
+
+                    vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+                    vim.keymap.set("n", "gf", ":lua require('conform').format()<cr>", { desc = "LSP: Format" })
+                    vim.keymap.set("n", "gn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
+                    vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+
+                    local filetype = vim.bo[bufnr].filetype
+                    if disable_semantic_tokens[filetype] then
+                        client.server_capabilities.semanticTokensProvider = nil
+                    end
+
+                    if server_capabilities then
+                        for k, v in pairs(server_capabilities) do
+                            if v == vim.NIL then
+                                v = nil
+                            end
+
+                            client.server_capabilities[k] = v
+                        end
+                    end
+                end,
+            })
+
+            vim.diagnostic.config({ virtual_text = false, })
+        end
+    end,
 }
