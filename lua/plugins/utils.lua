31d80a3fa5b7ac1042b1ea6ddcf6d259aca34a04
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-06-10 08:04:19 +0200

    Multicursor

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,5 +1,33 @@
 return {
     {
+        "jake-stewart/multicursor.nvim",
+        lazy = true,
+        branch = "1.0",
+        config = function()
+            local mc = require("multicursor-nvim")
+            mc.setup()
+
+            vim.api.nvim_set_hl(0, "MultiCursorCursor", { reverse = true })
+            vim.api.nvim_set_hl(0, "MultiCursorVisual", { link = "Visual" })
+            vim.api.nvim_set_hl(0, "MultiCursorSign", { link = "SignColumn"})
+            vim.api.nvim_set_hl(0, "MultiCursorMatchPreview", { link = "Search" })
+            vim.api.nvim_set_hl(0, "MultiCursorDisabledCursor", { reverse = true })
+            vim.api.nvim_set_hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+            vim.api.nvim_set_hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+        end,
+        keys = {
+            -- NOTE: multicursor.nvim is able to add/skip a cursor based on a search result
+            { "mj", function() require("multicursor-nvim").lineAddCursor(1) end},
+            { "mj", function() require("multicursor-nvim").lineAddCursor(1) end, mode="x"},
+            { "mk", function() require("multicursor-nvim").lineAddCursor(-1) end},
+            { "mk", function() require("multicursor-nvim").lineAddCursor(-1) end, mode="x"},
+            { "mc", function() require("multicursor-nvim").clearCursors() end},
+            { "mc", function() require("multicursor-nvim").clearCursors() end, mode="x"},
+            { "mm", function() require("multicursor-nvim").matchCursors() end},
+            { "mm", function() require("multicursor-nvim").matchCursors() end, mode="x"},
+        },
+    },
+    {
         "terrortylor/nvim-comment",
         lazy = true,
         config = function()
