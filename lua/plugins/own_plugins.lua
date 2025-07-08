3e266e893dcfeef38e85142d91a1171c06f86f34
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-07-08 06:56:34 +0200

    improve structure

---
+++ lua/plugins/own_plugins.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        'RaphaeleL/hartoon',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-telescope/telescope-fzf-native.nvim',
+        },
+        config = function()
+            require('hartoon').setup()
+        end,
+        keys = {
+            { '<leader>ht', '<cmd>HartoonTmuxSessions<cr>', desc = 'List tmux sessions' },
+            { '<leader>hta', '<cmd>HartoonPinTmuxSession<cr>', desc = 'Pin current tmux session' },
+            { '<leader>htm', '<cmd>HartoonEditPinnedTmux<cr>', desc = 'Edit pinned tmux sessions' },
+            { '<leader>ta', '<cmd>HartoonJumpTo1<cr>', desc = 'Jump to pinned tmux session 1' },
+            { '<leader>ts', '<cmd>HartoonJumpTo2<cr>', desc = 'Jump to pinned tmux session 2' },
+            { '<leader>td', '<cmd>HartoonJumpTo3<cr>', desc = 'Jump to pinned tmux session 3' },
+        },
+    }
+}
