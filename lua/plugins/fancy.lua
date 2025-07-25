a22cb140b53c2dd35e1610a8c099c2820ce109da
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-07-25 15:13:31 +0200

    fancy ui plugins

--- lua/plugins/fancy.lua
+++ lua/plugins/fancy.lua
@@ -63,69 +63,69 @@ return {
             { "<Leader>gs", ":Gitsigns toggle_signs<cr>",   desc = " Gitsigns: [G]it [t]oggle Signs" },
         },
     },
-    -- {
-    --     "lukas-reineke/indent-blankline.nvim",
-    --     main = "ibl",
-    --     -- lazy = true,
-    --     event = { "BufReadPost", "BufNewFile" },
-    --     config = function()
-    --         if vim.fn.has("nvim-0.10.0") == 0 then
-    --             local utils = require("ibl.utils")
-    --             ---@diagnostic disable-next-line: deprecated
-    --             utils.tbl_join = vim.tbl_flatten
-    --         end
-    --         require("ibl").setup({
-    --             indent = {
-    --                 char = "│",
-    --                 tab_char = "│",
-    --             },
-    --             scope = {
-    --                 enabled = false,
-    --                 show_start = false,
-    --                 show_end = false,
-    --                 injected_languages = false,
-    --                 show_exact_scope = false,
-    --             },
-    --             exclude = {
-    --                 filetypes = {
-    --                     "alpha",
-    --                     "checkhealth",
-    --                     "dashboard",
-    --                     "git",
-    --                     "gitcommit",
-    --                     "help",
-    --                     "lazy",
-    --                     "lazyterm",
-    --                     "lspinfo",
-    --                     "man",
-    --                     "mason",
-    --                     "neo-tree",
-    --                     "notify",
-    --                     "Outline",
-    --                     "TelescopePrompt",
-    --                     "TelescopeResults",
-    --                     "terminal",
-    --                     "toggleterm",
-    --                     "Trouble",
-    --                 },
-    --             },
-    --         })
-    --     end,
-    -- },
-    -- {
-    --     "RRethy/vim-illuminate",
-    --     -- lazy = true,
-    --     -- event = { "BufReadPost", "BufNewFile" },
-    --     event = { "VeryLazy" },
-    --     opts = {
-    --         delay = 0,
-    --         large_file_cutoff = 2000,
-    --         large_file_overrides = {
-    --             providers = { "lsp" },
-    --         },
-    --     },
-    --     config = function(_, opts)
-    --         require("illuminate").configure(opts)
-    --     end,
-    -- },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        -- lazy = true,
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            if vim.fn.has("nvim-0.10.0") == 0 then
+                local utils = require("ibl.utils")
+                ---@diagnostic disable-next-line: deprecated
+                utils.tbl_join = vim.tbl_flatten
+            end
+            require("ibl").setup({
+                indent = {
+                    char = "│",
+                    tab_char = "│",
+                },
+                scope = {
+                    enabled = false,
+                    show_start = false,
+                    show_end = false,
+                    injected_languages = false,
+                    show_exact_scope = false,
+                },
+                exclude = {
+                    filetypes = {
+                        "alpha",
+                        "checkhealth",
+                        "dashboard",
+                        "git",
+                        "gitcommit",
+                        "help",
+                        "lazy",
+                        "lazyterm",
+                        "lspinfo",
+                        "man",
+                        "mason",
+                        "neo-tree",
+                        "notify",
+                        "Outline",
+                        "TelescopePrompt",
+                        "TelescopeResults",
+                        "terminal",
+                        "toggleterm",
+                        "Trouble",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "RRethy/vim-illuminate",
+        -- lazy = true,
+        -- event = { "BufReadPost", "BufNewFile" },
+        event = { "VeryLazy" },
+        opts = {
+            delay = 0,
+            large_file_cutoff = 2000,
+            large_file_overrides = {
+                providers = { "lsp" },
+            },
+        },
+        config = function(_, opts)
+            require("illuminate").configure(opts)
+        end,
+    },
 }
