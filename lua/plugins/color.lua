c46b677b47e693ee65299074959382ee700750e1
LICCIARDO Raphaele Salvatore <raphaele.licciardo@frequentis.com>
2025-07-28 07:00:19 +0200

    no

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -22,46 +22,46 @@ return {
 	--         on_colors = function(colors) end,
 	--     },
 	-- },
-	{
-		"vague2k/vague.nvim",
-		-- lazy = true,
-		config = function()
-			require("vague").setup({
-				transparent = true,
-			})
-			vim.cmd("colorscheme vague")
-			vim.cmd("hi statusline guibg=NONE")
-		end,
-	},
 	-- {
-	--     "rose-pine/neovim",
-	--     lazy = true,
-	--     config = function()
-	--         require("rose-pine").setup({
-	--             variant = "auto",
-	--             dark_variant = "moon",
-	--             dim_inactive_windows = true,
-	--             extend_background_behind_borders = true,
-	--             dim_nc_background = true,
-	--             disable_background = true,
-	--             styles = {
-	--                 bold = true,
-	--                 italic = true,
-	--                 transparency = true,
-	--             },
-	--             groups = {},
-	--             highlight_groups = {
-	--                 Normal = { bg = "NONE" },
-	--                 NormalNC = { bg = "NONE" },
-	--                 NormalFloat = { bg = "NONE" },
-	--                 StatusLine = { bg = "NONE" },
-	--                 StatusLineNC = { bg = "NONE" },
-	--                 CursorLineNr = { fg = "gold" },
-	--             },
-	--         })
-	--         vim.cmd("colorscheme rose-pine")
-	--     end,
+	-- 	"vague2k/vague.nvim",
+	-- 	-- lazy = true,
+	-- 	config = function()
+	-- 		require("vague").setup({
+	-- 			transparent = true,
+	-- 		})
+	-- 		vim.cmd("colorscheme vague")
+	-- 		vim.cmd("hi statusline guibg=NONE")
+	-- 	end,
 	-- },
+	{
+	    "rose-pine/neovim",
+	    lazy = true,
+	    config = function()
+	        require("rose-pine").setup({
+	            variant = "auto",
+	            dark_variant = "moon",
+	            dim_inactive_windows = true,
+	            extend_background_behind_borders = true,
+	            dim_nc_background = true,
+	            disable_background = true,
+	            styles = {
+	                bold = true,
+	                italic = true,
+	                transparency = true,
+	            },
+	            groups = {},
+	            highlight_groups = {
+	                Normal = { bg = "NONE" },
+	                NormalNC = { bg = "NONE" },
+	                NormalFloat = { bg = "NONE" },
+	                StatusLine = { bg = "NONE" },
+	                StatusLineNC = { bg = "NONE" },
+	                CursorLineNr = { fg = "gold" },
+	            },
+	        })
+	        vim.cmd("colorscheme rose-pine")
+	    end,
+	},
 	-- {
 	--     "rebelot/kanagawa.nvim",
 	--     -- lazy = true,
